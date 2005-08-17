0013b100366d4b6fb512e02952d504d96192da8d
Unknown Perforce User <marcmac>
2005-08-17 02:43:33 -0500

    fix legacy package removal
    Copied from Perforce
     Change: 11097

--- rpmconf/Install/install.sh
+++ rpmconf/Install/install.sh
@@ -472,7 +472,7 @@ removeExistingInstall() {
 		echo "Removing existing packages"
 		echo ""
 
-		rpm -ev --noscripts --allmatches zimbra-core zimbra-snmp zimbra-ldap zimbra-mta zimbra-store > /dev/null 2>&1
+		rpm -ev --noscripts --allmatches liquid-core liquid-snmp liquid-ldap liquid-mta liquid-store > /dev/null 2>&1
 
 		for p in $INSTALLED_PACKAGES; do
 			echo -n "   $p..."
