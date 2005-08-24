70e922e13ee1599ed1ad5635bb187a87be77931f
Unknown Perforce User <marcmac>
2005-08-24 13:48:43 -0500

    Integrate specific change numbers
    Copied from Perforce
     Change: 11500

---
+++ RE/p4changeintegrate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+TOBRANCH=$1
+shift
+
+if [ x$TOBRANCH = "x" -o $# -eq 0 ]; then
+	echo $0 '<to branch> <change> [<change>...]'
+fi
+
+while [ $# -gt 0 ]; do
+	echo "Integrating change $1"
+
+	p4 describe -s $1 | egrep '^\.\.\.' > /tmp/change.$$ 2>&1
+	if [ $? -gt 0 ]; then
+		echo "Change $1 not found!"
+	else
+		while read i; do
+			SOURCEFILE=`echo $i | awk '{print $2}'`
+			DESTFILE=`echo $SOURCEFILE | sed -e "s|//depot/[^/]*|//depot/$TOBRANCH|"`
+			#echo "file $SOURCEFILE"
+			#cho "	$DESTFILE"
+			p4 integrate $SOURCEFILE $DESTFILE
+		done < /tmp/change.$$
+	fi
+	rm /tmp/change.$$
+
+	shift
+done
+
+p4 resolve
+
+p4 submit
+
