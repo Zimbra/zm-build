745e46aa0e0bc1ca113258ed7808d3aebdbb1b5d
Unknown Perforce User <marcmac>
2005-11-21 15:07:05 -0600

    Fix install history
    Copied from Perforce
     Change: 17067

--- rpmconf/Install/Util/modules/packages.sh
+++ rpmconf/Install/Util/modules/packages.sh
@@ -31,10 +31,15 @@ installPackage() {
 	f=`basename $file`
 	echo -n "...$f..."
 	$PACKAGEINST $file >> $LOGFILE 2>&1
+	if [ $UPGRADE = "yes" ]; then
+		ST="UPGRADED"
+	else
+		ST="INSTALLED"
+	fi
 	D=`date +%s`
 	if [ $? = 0 ]; then
 		echo "done"
-		echo "${D}: INSTALLED $f" >> /opt/zimbra/.install_history
+		echo "${D}: $ST $f" >> /opt/zimbra/.install_history
 	else
 		echo -n "FAILED"
 		echo ""
