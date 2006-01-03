e1d799924722ee6a4a0a91029d6622a90c57d7cb
Unknown Perforce User <marcmac>
2006-01-03 14:13:53 -0600

    Fix mta auth host set sub.
    Copied from Perforce
     Change: 18992

--- rpmconf/Install/zmsetup.pl
+++ rpmconf/Install/zmsetup.pl
@@ -737,8 +737,8 @@ sub setSmtpHost {
 }
 
 sub setMtaAuthHost {
-	changeLdapHost( askNonBlank("Please enter the mta authentication server hostname",
-			$config{MTAAUTHHOST}));
+	$config{MTAAUTHHOST} = askNonBlank("Please enter the mta authentication server hostname",
+			$config{MTAAUTHHOST});
 }
 
 sub setLdapHost {
