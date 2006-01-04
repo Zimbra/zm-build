1d3b2d716ca4ddfe71df01777243bb7ff8fd2a3d
Unknown Perforce User <marcmac>
2006-01-03 18:37:39 -0600

    remove notification from network
    Copied from Perforce
     Change: 19034

--- rpmconf/Install/postinstall.pm
+++ rpmconf/Install/postinstall.pm
@@ -31,6 +31,25 @@ sub configure {
 	if (main::isEnabled("zimbra-ldap")) {
 		main::runAsZimbra ("zmprov mcf zimbraComponentAvailable ''");
 	}
+
+	if (!defined ($main::options{c}) && 1) {
+		if (askYN("\nYou have the option of notifying Zimbra of your installation.\nThis helps us to track the uptake of the Zimbra Collaboration Suite.\nThe only information that will be transmitted is:\n\tThe VERSION of zcs you've installed($main::curVersion)\n\tThe ADMIN EMAIL ADDRESS you've created ($main::config{CREATEADMIN})\n\nNotify Zimbra of your installation?", "Yes") eq "yes") {
+			if (open NOTIFY, "/opt/zimbra/libexec/zmnotifyinstall $main::curVersion $main::config{CREATEADMIN} |") {
+				while (<NOTIFY>) {
+					progress ("$_");
+				}
+				close NOTIFY;
+				progress ("Notification complete!\n");
+			} else {
+				progress ("ERROR: Notification failed!\n\n");
+			}
+		} else {
+		progress ("Notification skipped\n");
+	}
+
+}
+
+
 }
 
 1
