839027bba2d3425b8ced80cb8bcf48018fbf81f1
Unknown Perforce User <marcmac>
2006-01-10 17:23:40 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=5128 bug: 5128
    Copied from Perforce
     Change: 19337

--- rpmconf/Install/Util/utilfunc.sh
+++ rpmconf/Install/Util/utilfunc.sh
@@ -496,6 +496,7 @@ restoreExistingConfig() {
 			runAsZimbra "zmlocalconfig -f -e $i"
 		done < $RF
 		if [ -f $RESTORECONFIG/backup.save ]; then
+			echo -n "Restoring backup schedule..."
 			runAsZimbra "cat $RESTORECONFIG/backup.save | xargs zmschedulebackup -R"
 		fi
 		echo "done"
