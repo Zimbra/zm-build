13a881d75fbb056bc35012aac94dccd3e833c588
Unknown Perforce User <marcmac>
2006-02-06 14:38:02 -0600

    Fix mac build to pick up version numbers automatically
    Copied from Perforce
     Change: 20962

--- rpmconf/Build/create_distribution.dist.sh
+++ rpmconf/Build/create_distribution.dist.sh
@@ -12,6 +12,9 @@ get_size_from_pkg() {
 
 get_build_num() {
 	DIR=`dirname $0`
+	MAJOR=`cat $DIR/../../RE/MAJOR`
+	MINOR=`cat $DIR/../../RE/MINOR`
+	MICRO=`cat $DIR/../../RE/MICRO`
 	BUILDNUM=`cat $DIR/../../RE/BUILD`
 }
 
@@ -31,6 +34,9 @@ cat $TEMPLATE | sed -e "s/@@CORESIZE@@/$CORESIZE/g" \
 	-e "s/@@MTASIZE@@/$MTASIZE/g" \
 	-e "s/@@SNMPSIZE@@/$SNMPSIZE/g" \
 	-e "s/@@SPELLSIZE@@/$SPELLSIZE/g" \
+	-e "s/@@MAJOR@@/$MAJOR/g" \
+	-e "s/@@MINOR@@/$MINOR/g" \
+	-e "s/@@MICRO@@/$MICRO/g" \
 	-e "s/@@BUILDNUM@@/$BUILDNUM/g" 
 
 
