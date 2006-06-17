e70d16eeb5e415b850d2734669b74eb9a63c698f
Unknown Perforce User <anandp>
2006-06-17 04:02:34 -0500

    bug: none fix copyright notice
    Copied from Perforce
     Change: 27147

--- rpmconf/Install/preinstall.pm
+++ rpmconf/Install/preinstall.pm
@@ -16,7 +16,7 @@
 # The Original Code is: Zimbra Collaboration Suite Server.
 # 
 # The Initial Developer of the Original Code is Zimbra, Inc.
-# Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+# Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
 # All Rights Reserved.
 # 
 # Contributor(s):
