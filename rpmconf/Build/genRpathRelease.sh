dc84aa94c45e50e971281aeaf761d3d9f51e4677
Unknown Perforce User <brian>
2006-10-05 13:46:06 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=7457 bug: 7457
    Copied from Perforce
     Change: 32933

--- rpmconf/Build/genRpathRelease.sh
+++ rpmconf/Build/genRpathRelease.sh
@@ -11,12 +11,12 @@ cvc cook group-dist
 cd $BUILDROOT
 
 TROVE=`conary rq --full-versions --flavors group-dist=/zimbra.rpath.org@rpl:1//zimbra.liquidsys.com@zimbra:devel`
-BUILD=`rbuilder build-create zcs-${RELEASETAG} "$TROVE" installable_iso --wait | awk -F= '{print $NF}'`
+BUILD=`rbuilder build-create zimbra "$TROVE" installable_iso --wait | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
   ISO=`rbuilder build-url $BUILD | head -1`
   wget -o $BUILDROOT/i386/zcs-${RELEASETAG}.iso $ISO
 fi
-BUILD=`rbuilder build-create zcs-${RELEASETAG} "$TROVE" vmware_image --wait | awk -F= '{print $NF}'`
+BUILD=`rbuilder build-create zimbra "$TROVE" vmware_image --wait | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
   ISO=`rbuilder build-url $BUILD | head -1`
   wget -o $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip $ISO
