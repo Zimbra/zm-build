cb32628f812a36436556ab2269d6d415e2674173
Unknown Perforce User <brian>
2006-10-04 21:02:55 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=7457 bug: 7457  rpath release targets and scripts
    Copied from Perforce
     Change: 32884

---
+++ rpmconf/Build/genRpathRelease.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+BUILDROOT=$1
+RELEASETAG=$2
+
+cd $BUILDROOT
+cvc checkout group-dist
+cd group-dist
+cvc cook group-dist
+cd $BUILDROOT
+
+TROVE=`conary rq --full-versions --flavors group-dist=/zimbra.rpath.org@rpl:1//zimbra.liquidsys.com@zimbra:devel`
+BUILD=`rbuilder build-create zcs-${RELEASETAG} "$TROVE" installable_iso --wait | awk -F= '{print $NF}'`
+if [ $? -eq 0 ]; then
+  ISO=`rbuilder build-url $BUILD | head -1`
+  wget -o $BUILDROOT/i386/zcs-${RELEASETAG}.iso $ISO
+fi
+BUILD=`rbuilder build-create zcs-${RELEASETAG} "$TROVE" vmware_image --wait | awk -F= '{print $NF}'`
+if [ $? -eq 0 ]; then
+  ISO=`rbuilder build-url $BUILD | head -1`
+  wget -o $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip $ISO
+fi
