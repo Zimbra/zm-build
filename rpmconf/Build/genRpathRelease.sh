38f07e75beed7030733a89d61ebe292b009ec9d7
Unknown Perforce User <brian>
2006-10-24 13:17:41 -0500

    bug: none
     add symlinks to zcs.iso and zcs-vmware.zip for the release files
    
    Copied from Perforce
     Change: 34149

--- rpmconf/Build/genRpathRelease.sh
+++ rpmconf/Build/genRpathRelease.sh
@@ -15,9 +15,11 @@ BUILD=`rbuilder build-create zimbra "$TROVE" installable_iso --wait | awk -F= '{
 if [ $? -eq 0 ]; then
   ISO=`rbuilder build-url $BUILD | head -1`
   wget -qO $BUILDROOT/i386/zcs-${RELEASETAG}.iso $ISO
+  ln -s $BUILDROOT/i386/zcs-${RELEASETAG}.iso $BUILDROOT/i386/zcs.iso
 fi
 BUILD=`rbuilder build-create zimbra "$TROVE" vmware_image --wait | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
   ISO=`rbuilder build-url $BUILD | head -1`
   wget -qO $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip $ISO
+  ln -s $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip $BUILDROOT/i386/zcs-vmware.zip
 fi
