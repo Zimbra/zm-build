7ec4075c87f130d2f5854bbae6c4f5f3ecb6d9e8
Unknown Perforce User <brian>
2006-11-07 17:58:10 -0600

    bug: none
    Copied from Perforce
     Change: 35064

--- rpmconf/Build/genRpathRelease.sh
+++ rpmconf/Build/genRpathRelease.sh
@@ -13,7 +13,7 @@ elif [ "$1" = "--label" ]; then
 fi
 
 BUILDROOT=$1
-RELEASETAG=$2
+RELEASETAG=$4
 
 cd $BUILDROOT
 cvc checkout group-dist=$LABEL
@@ -27,22 +27,22 @@ fi
 cd $BUILDROOT
 
 TROVE=`conary rq --full-versions --flavors group-dist=/zimbra.rpath.org@rpl:1//$LABEL`
-echo "Building ISO Image $BUILDROOT/i386/zcs-${RELEASETAG}.iso..."
+echo "Building ISO Image $BUILDROOT/zcs-${RELEASETAG}.iso..."
 BUILD=`rbuilder build-create zimbra "$TROVE" installable_iso --wait | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
   echo "Getting URL for Build $BUILD"
   ISO=`rbuilder build-url $BUILD | head -1`
   echo "Retrieving image from $ISO"
-  wget -qO $BUILDROOT/i386/zcs-${RELEASETAG}.iso $ISO
-  ln -s $BUILDROOT/i386/zcs-${RELEASETAG}.iso $BUILDROOT/i386/zcs.iso
+  wget -qO $BUILDROOT/zcs-${RELEASETAG}.iso $ISO
+  ln -s $BUILDROOT/zcs-${RELEASETAG}.iso $BUILDROOT/zcs.iso
 fi
 
-echo "Building VMWare Image $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip..."
-BUILD=`rbuilder build-create zimbra "$TROVE" vmware_image --wait --option 'vmMemory 512' 'freespace 500'  | awk -F= '{print $NF}'`
+echo "Building VMWare Image $BUILDROOT/zcs-${RELEASETAG}-vmware.zip..."
+BUILD=`rbuilder build-create zimbra "$TROVE" vmware_image --wait --option 'vmMemory 512' --option 'freespace 500'  | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
   echo "Getting URL for Build $BUILD"
   ISO=`rbuilder build-url $BUILD | head -1`
   echo "Retrieving image from $ISO"
-  wget -qO $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip $ISO
-  ln -s $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip $BUILDROOT/i386/zcs-vmware.zip
+  wget -qO $BUILDROOT/zcs-${RELEASETAG}-vmware.zip $ISO
+  ln -s $BUILDROOT/zcs-${RELEASETAG}-vmware.zip $BUILDROOT/zcs-vmware.zip
 fi
