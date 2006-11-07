c2e069d202247fe78b882b1d3e91f4e8bace2025
Unknown Perforce User <brian>
2006-11-07 15:58:30 -0600

    bug: none
    Copied from Perforce
     Change: 35046

--- rpmconf/Build/genRpathRelease.sh
+++ rpmconf/Build/genRpathRelease.sh
@@ -30,14 +30,19 @@ TROVE=`conary rq --full-versions --flavors group-dist=/zimbra.rpath.org@rpl:1//$
 echo "Building ISO Image $BUILDROOT/i386/zcs-${RELEASETAG}.iso..."
 BUILD=`rbuilder build-create zimbra "$TROVE" installable_iso --wait | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
+  echo "Getting URL for Build $BUILD"
   ISO=`rbuilder build-url $BUILD | head -1`
+  echo "Retrieving image from $ISO"
   wget -qO $BUILDROOT/i386/zcs-${RELEASETAG}.iso $ISO
   ln -s $BUILDROOT/i386/zcs-${RELEASETAG}.iso $BUILDROOT/i386/zcs.iso
 fi
+
 echo "Building VMWare Image $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip..."
 BUILD=`rbuilder build-create zimbra "$TROVE" vmware_image --wait --option 'vmMemory 512' 'freespace 500'  | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
+  echo "Getting URL for Build $BUILD"
   ISO=`rbuilder build-url $BUILD | head -1`
+  echo "Retrieving image from $ISO"
   wget -qO $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip $ISO
   ln -s $BUILDROOT/i386/zcs-${RELEASETAG}-vmware.zip $BUILDROOT/i386/zcs-vmware.zip
 fi
