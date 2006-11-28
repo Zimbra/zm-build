b78d33ffd10d7f29243df31d9b110baa3acfa52b
Unknown Perforce User <brian>
2006-11-27 23:21:01 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=11420 http://bugzilla.zimbra.com/show_bug.cgi?id=12187 bug: 11420, 12187  zimbraFeatureIdentitiesEnabled = True for all COS's
    Copied from Perforce
     Change: 36550

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -1072,7 +1072,7 @@ sub upgrade450RC1 {
 	  my @coses = `su - zimbra -c "$ZMPROV gac"`;
 	  foreach my $cos (@coses) {
 		  chomp $cos;
-		  main::runAsZimbra("$ZMPROV mc $cos zimbraFeaturePop3DataSourceEnabled TRUE zimbraPrefReadingPaneEnabled TRUE zimbraPrefUseRfc2231 FALSE");
+		  main::runAsZimbra("$ZMPROV mc $cos zimbraFeaturePop3DataSourceEnabled TRUE zimbraPrefReadingPaneEnabled TRUE zimbraPrefUseRfc2231 FALSE zimbraFeatureIdentitiesEnabled TRUE");
 	  }
 
     # bah-bye timezones
