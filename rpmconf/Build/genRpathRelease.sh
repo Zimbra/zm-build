9335801a43f497592f76aae9694571209a0d0989
Unknown Perforce User <brian>
2006-12-11 21:28:26 -0600

    bug: none  better error checking when generating the iso and vmware image  pass the rbuilder label so we can build both internal and production  builds with the same script.
    Copied from Perforce
     Change: 37701

--- rpmconf/Build/genRpathRelease.sh
+++ rpmconf/Build/genRpathRelease.sh
@@ -4,7 +4,7 @@
 LOCAL=0
 LABEL=zimbra.liquidsys.com@zimbra:zcs
 #group-dist=/zimbra.rpath.org@rpl:1//$LABEL
-group-dist=/$LABEL
+#group-dist=/$LABEL
 
 if [ "$1" = "--local" ]; then
     LOCAL=1
@@ -18,7 +18,11 @@ BUILDROOT=$1
 RELEASETAG=$4
 
 cd $BUILDROOT
-cvc checkout group-dist=$LABEL
+cvc checkout group-dist=$LABEL 2> /dev/null
+if [ $? != 0 ]; then
+  echo "cvc checkout group-dist=$LABEL failed"
+  exit 1
+fi
 cd group-dist
 cvc cook group-dist=$LABEL --debug
 if [ $? -ne 0 ]; then
@@ -28,23 +32,37 @@ fi
 
 cd $BUILDROOT
 
-TROVE=`conary rq --full-versions --flavors $group-dist`
+TROVE=`conary rq --full-versions --flavors group-dist=/$LABEL`
+if [ $? != 0 ]; then
+  exit 1
+fi
 echo "Building ISO Image $BUILDROOT/zcs-${RELEASETAG}.iso..."
 BUILD=`rbuilder build-create zimbra "$TROVE" installable_iso --wait | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
   echo "Getting URL for Build $BUILD"
   ISO=`rbuilder build-url $BUILD | head -1`
   echo "Retrieving image from $ISO"
-  wget -qO $BUILDROOT/zcs-${RELEASETAG}.iso $ISO
-  ln -s $BUILDROOT/zcs-${RELEASETAG}.iso $BUILDROOT/zcs.iso
+  if [ x"$ISO" != "x" ]; then
+    wget -qO $BUILDROOT/zcs-${RELEASETAG}.iso $ISO
+    ln -s $BUILDROOT/zcs-${RELEASETAG}.iso $BUILDROOT/zcs.iso
+  fi
 fi
 
 echo "Building VMWare Image $BUILDROOT/zcs-${RELEASETAG}-vmware.zip..."
 BUILD=`rbuilder build-create zimbra "$TROVE" vmware_image --wait --option 'vmMemory 512' --option 'freespace 500'  | awk -F= '{print $NF}'`
 if [ $? -eq 0 ]; then
   echo "Getting URL for Build $BUILD"
-  ISO=`rbuilder build-url $BUILD | head -1`
-  echo "Retrieving image from $ISO"
-  wget -qO $BUILDROOT/zcs-${RELEASETAG}-vmware.zip $ISO
-  ln -s $BUILDROOT/zcs-${RELEASETAG}-vmware.zip $BUILDROOT/zcs-vmware.zip
+  ZIP=`rbuilder build-url $BUILD | head -1`
+  if [ x"$ZIP" != "x" ]; then
+    echo "Retrieving image from $ZIP"
+    wget -qO $BUILDROOT/zcs-${RELEASETAG}-vmware.zip $ZIP
+    ln -s $BUILDROOT/zcs-${RELEASETAG}-vmware.zip $BUILDROOT/zcs-vmware.zip
+  fi
 fi
+
+if [ "x$ISO" = "x" -o "x$ZIP" = "x" ]; then
+  exit 1
+else 
+  exit 0
+fi
+  
