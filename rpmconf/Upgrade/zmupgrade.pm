3ff8089720bc793522e0e2a6352862efbb7ff957
Quanah Gibson-Mount <quanah@zimbra.com>
2007-10-11 19:12:15 -0500

    bug: 20936 Fix missing next line
    http://bugzilla.zimbra.com/show_bug.cgi?id=20936
    
    Copied from Perforce
     Change: 70197

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -1804,6 +1804,7 @@ sub migrateAmavisDB($) {
   my $toDir = "${amavisdBase}-$toVersion";
   main::progress("Migrating amavisd-new to version $toVersion\n");
   foreach my $fromVersion qw(2.5.2 2.4.3 2.4.1 2.3.3 2.3.1) {
+    next if ($toVersion eq $fromVersion);
     my $fromDir = "${amavisdBase}-$fromVersion";
     main::progress("Checking $fromDir/db\n");
     if ( -d "$fromDir/db" && -d "$toDir" && ! -e "$toDir/db/cache.db") {
