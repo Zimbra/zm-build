9dd387de44e83b5997f0324f9c11eec08cdbc6f6
Quanah Gibson-Mount <quanah@zimbra.com>
2008-07-24 23:37:49 -0500

    Integrated the following from //depot/main/...: Change 108251 by quanah@qmain on 2008/07/24 21:37:38
    bug: none
    Fix typo
    
    Copied from Perforce
     Change: 108252

--- rpmconf/Install/zmsetup.pl
+++ rpmconf/Install/zmsetup.pl
@@ -534,6 +534,9 @@ sub isEnabled {
       if (isInstalled($p) and not defined $prevInstalledPackages{$p}) {
         detail("Marking $p as installed. Services for $p will be enabled.");
         $enabledPackages{$p} = "Enabled";
+        if ($p eq "zimbra-convertd") {
+          $enabledPackages{$p} = "Disabled";
+        }
       } elsif (isInstalled($p) and not defined $enabledPackages{$p}) {
         detail("Marking $p as disabled.");
         $enabledPackages{$p} = "Disabled";
@@ -544,6 +547,7 @@ sub isEnabled {
     detail("Newinstall enabling all installed packages");
     foreach my $p (@packageList) {
       if (isInstalled($p)) {
+        $enabledPackages{"zimbra-convertd"} = "Disabled";
         unless ($enabledPackages{$p} eq "Disabled") {
           detail("Enabling $p");
           $enabledPackages{$p} = "Enabled" 
@@ -3111,8 +3115,9 @@ sub createMainMenu {
     if ($package eq "zimbra-core") {next;}
     if ($package eq "zimbra-apache") {next;}
     if ($package eq "zimbra-archiving") {next;}
+    if ($package eq "zimbra-convertd") {next;}
     if (defined($installedPackages{$package})) {
-      if ($package =~ /logger|spell|convertd/) {
+      if ($package =~ /logger|spell/) {
         $mm{menuitems}{$i} = { 
           "prompt" => "$package:", 
           "var" => \$enabledPackages{$package},
