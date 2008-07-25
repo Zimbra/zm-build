1a3d1b120b8cf5472505759c5d2260e48926cea3
Unknown Perforce User <brian>
2008-07-25 14:55:08 -0500

    bug: none
    Copied from Perforce
     Change: 108331

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -2645,8 +2645,8 @@ sub updateLoggerMySQLcnf {
         next;
       } elsif (/^thread_cache/) {
         # 29475 fix thread_cache_size
-        my $fix = 's/^thread_cache/thread_cache_size/g';
-        print TMP $fix;
+        s/^thread_cache/thread_cache_size/g;
+        print TMP;
         $mycnfChanged=1;
         next;
       } elsif (/^skip-external-locking/) {
@@ -2696,8 +2696,8 @@ sub updateMySQLcnf {
         next;
       } elsif (/^thread_cache/) {
         # 29475 fix thread_cache_size
-        my $fix = 's/^thread_cache/thread_cache_size/g';
-        print TMP $fix;
+        s/^thread_cache/thread_cache_size/g;
+        print TMP;
         $mycnfChanged=1;
         next;
       } elsif (/^skip-external-locking/) {
