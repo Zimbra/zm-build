240fdd332776ce422ab898d46d6473b1877c2c72
Quanah Gibson-Mount <quanah@zimbra.com>
2009-02-06 14:03:44 -0600

    bug: 34738 Add option to build network edition Correctly set the build targets
    http://bugzilla.zimbra.com/show_bug.cgi?id=34738
    
    Copied from Perforce
     Change: 144594

--- buildZCS.sh
+++ buildZCS.sh
@@ -4,16 +4,21 @@ PROGDIR=`dirname $0`
 cd $PROGDIR
 PATHDIR=`pwd`
 BUILDTHIRDPARTY=no
-RELEASE=main
+BUILDNETWORK=no
 
 usage() {
 	echo ""
-	echo "Usage: "`basename $0`" -t" >&2
+	echo "Usage: "`basename $0`" [-t] [-n]" >&2
+	echo "-n: Perform a Network Edition build"
 	echo "-t: Build third party as well as ZCS"
 }
 
 while [ $# -gt 0 ]; do
 	case $1 in
+		-n|--network)
+			BUILDNETWORK=yes
+			shift;
+			;;
 		-t|--thirdparty)
 			BUILDTHIRDPARTY=yes
 			shift;
@@ -27,13 +32,13 @@ done
 
 RELEASE=${PATHDIR%/*}
 RELEASE=${RELEASE##*/}
-#echo "RELEASE $RELEASE"
 
 if [ x$BUILDTHIRDPARTY = x"yes" ]; then
 	if [ -x "../ThirdParty/buildThirdParty.sh" ]; then
-		RC=`${PATHDIR}/../ThirdParty/buildThirdParty.sh -c -r ${RELEASE}`;
+		RC=`${PATHDIR}/../ThirdParty/buildThirdParty.sh -c`;
 		if [ RC != 0 ]; then
 			echo "Error: Building third party failed"
+			echo "Please fix and retry"
 			exit 1;
 
 	else
@@ -41,7 +46,29 @@ if [ x$BUILDTHIRDPARTY = x"yes" ]; then
 		exit 1;
 	fi
 fi
+TARGETS="sourcetar all"
 
-cd $PATHDIR
-make -f Makefile ajaxtar sourcetar all
+if [ x$BUILDNETWORK = x"yes" ]; then
+	if [ -f "$PATHDIR/../ZimbraNetwork/ZimbraBuild/Makefile" ]; then
+		if [ x$RELEASE = x"main" ]; then
+			TARGETS="$TARGETS velodrome"
+		elif [[ $RELEASE == "FRANKLIN"* ]]; then
+			TARGETS="$TARGETS velodrome customercare"
+		fi
+	else
+		echo "Error: ZimbraNetwork is not available"
+		exit 1;
+	fi
+fi
+if [ x$BUILDNETWORK = x"no" ]; then
+	TARGETS="ajaxtar $TARGETS"
+fi
+
+if [ x$BUILDNETWORK = x"no" ]; then
+	cd $PATHDIR
+else
+	cd $PATHDIR/../ZimbraNetwork/ZimbraBuild/Makefile
+fi
+
+make -f Makefile $TARGETS
 exit 0;
