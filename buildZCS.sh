ed344cc81729ce874d3b255cde9f4f11783d20db
Quanah Gibson-Mount <quanah@zimbra.com>
2009-03-14 20:34:34 -0500

    bug: 34634 Update copyright and licenses
    http://bugzilla.zimbra.com/show_bug.cgi?id=34634
    
    Copied from Perforce
     Change: 149622

--- buildZCS.sh
+++ buildZCS.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # 
 # ***** BEGIN LICENSE BLOCK *****
-# 
 # Zimbra Collaboration Suite Server
-# Copyright (C) 2006 Zimbra, Inc.
+# Copyright (C) 2009 Zimbra, Inc.
 # 
 # The contents of this file are subject to the Yahoo! Public License
 # Version 1.0 ("License"); you may not use this file except in
@@ -12,7 +11,6 @@
 # 
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
-# 
 # ***** END LICENSE BLOCK *****
 # 
 
