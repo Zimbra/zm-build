592e91d208f59485e4afa52c521c824c713c7bf9
Unknown Perforce User <pfnguyen>
2009-04-21 20:24:54 -0500

    bug:37228
    zmlogger: use information from rrd_column_type table when creating rrd datasources (type:heartbeat)
    
    LoggerSchema.pm: add upgrade statements for schema version 1
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=37228
    
    Copied from Perforce
     Change: 154983

--- lib/Zimbra/Mon/LoggerSchema.pm
+++ lib/Zimbra/Mon/LoggerSchema.pm
@@ -32,19 +32,44 @@ our @LOGGER_SCHEMA_UPGRADE = (
       }
     ],
     # version 1, column mapping
-    #[ q{
-    #	CREATE TABLE column_type (
-    #	)
-    #  },
-    #  q{
-    #  	INSERT INTO column_type VALUES
-    #  	( ),
-    #  	( )
-    #  },
-    #  q{
-    #  	UPDATE config SET version = 1
-    #  }
-    #],
+    [ q{
+    	CREATE TABLE rrd_column_type (
+    	    csv_file     varchar(255) not null,
+    	    col_name     varchar(255) not null,
+    	    -- G-AUGE, C-OUNTER, A-BSOLUTE or D-ERIVED
+    	    col_type     char(1) not null,
+    	    col_interval integer,
+    	    CONSTRAINT unqcoltype UNIQUE (csv_file, col_name)
+    	)
+      },
+      # sqlite's version of multiple insert
+      #
+      # by default, all columns will have an interval of 30s
+      # and type of GAUGE (i.e. columns not in this table)
+      q{
+      	INSERT INTO rrd_column_type
+      	          select 'mailboxd.csv', 'gc_minor_ms',    'C', 30
+      	union all select 'mailboxd.csv', 'gc_minor_count', 'C', 30
+      	union all select 'mailboxd.csv', 'gc_major_ms',    'C', 30
+      	union all select 'mailboxd.csv', 'gc_major_count', 'C', 30
+      	union all select 'mysql.csv',    'Slow_queries',   'C', 30
+      	union all select 'mysql.csv',    'Opened_tables',  'C', 30
+      	union all select 'df.csv',       'disk_pct_used',  'G', 600
+      	union all select 'df.csv',       'disk_use',       'G', 600
+      	union all select 'df.csv',       'disk_space',     'G', 600
+      	union all select 'zmmtastats',   'clam_events',    'A', 300
+      	union all select 'zmmtastats',   'sendmail_events','A', 300
+      	union all select 'zmmtastats',   'filter_count',   'A', 300
+      	union all select 'zmmtastats',   'filter_virus',   'A', 300
+      	union all select 'zmmtastats',   'filter_spam',    'A', 300
+      	union all select 'zmmtastats',   'filter_misc',    'A', 300
+      	union all select 'zmmtastats',   'mta_count',      'A', 300
+      	union all select 'zmmtastats',   'mta_volume',     'A', 300
+      },
+      q{
+      	UPDATE config SET version = 1
+      }
+    ],
 );
 
 1;
