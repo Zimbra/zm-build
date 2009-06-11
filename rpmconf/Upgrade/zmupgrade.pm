551d41cb3f9431eeae92d4a1837f871fbac8898e
Unknown Perforce User <brian>
2009-06-11 16:08:06 -0500

    bug: none
    Copied from Perforce
     Change: 161283

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -2860,23 +2860,6 @@ sub upgrade600RC1 {
   
   main::runAsZimbra("zmjava com.zimbra.common.localconfig.LocalConfigUpgrade --bug 37842 --bug 37844 --bug 37802 --tag .pre.${targetVersion}");
 
-  if (main::isInstalled("zimbra-store")) {
-    my $mailboxd_java_options = main::getLocalConfig("mailboxd_java_options");
-    $mailboxd_java_options =~ s/-client/-server/
-      if ($mailboxd_java_options =~ /-client/);
-    $mailboxd_java_options .= " -XX:+UseConcMarkSweepGC"
-      unless ($mailboxd_java_options =~ /UseConcMarkSweepGC/);
-    $mailboxd_java_options .= " -XX:NewRatio"
-      unless ($mailboxd_java_options =~ /XX:NewRatio/);
-    $mailboxd_java_options .= " -XX:PermSize"
-      unless ($mailboxd_java_options =~ /XX:Permsize/);
-    $mailboxd_java_options .= " -XX:MaxPermSize"
-      unless ($mailboxd_java_options =~ /XX:MaxPermSize/);
-    $mailboxd_java_options .= " -XX:SoftRefLRUPolicyMSPerMB"
-      unless ($mailboxd_java_options =~ /XX:SoftRefLRUPolicyMSPerMB/);
-    main::detail("Modified mailboxd_java_options=$mailboxd_java_options");
-    main::setLocalConfig("mailboxd_java_options", "$mailboxd_java_options");
-  }
   return 0;
 }
 
