5c2733198492648f99f68d56f26934d0ba4513d1
Quanah Gibson-Mount <quanah@zimbra.com>
2009-06-19 20:18:50 -0500

    bug: none 5.0.18 bits
    Copied from Perforce
     Change: 162340

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -182,6 +182,7 @@ my %updateFuncs = (
   "5.0.15_GA" => \&upgrade5015GA,
   "5.0.16_GA" => \&upgrade5016GA,
   "5.0.17_GA" => \&upgrade5017GA,
+  "5.0.18_GA" => \&upgrade5018GA,
   "6.0.0_BETA1" => \&upgrade600BETA1,
   "6.0.0_BETA2" => \&upgrade600BETA2,
   "6.0.0_RC1" => \&upgrade600RC1,
@@ -252,6 +253,7 @@ my @versionOrder = (
   "5.0.15_GA",
   "5.0.16_GA",
   "5.0.17_GA",
+  "5.0.18_GA",
   "6.0.0_BETA1",
   "6.0.0_BETA2",
   "6.0.0_RC1",
@@ -429,6 +431,8 @@ sub upgrade {
     main::progress("This appears to be 5.0.16_GA\n");
   } elsif ($startVersion eq "5.0.17_GA") {
     main::progress("This appears to be 5.0.17_GA\n");
+  } elsif ($startVersion eq "5.0.18_GA") {
+    main::progress("This appears to be 5.0.18_GA\n");
   } elsif ($startVersion eq "6.0.0_BETA1") {
     main::progress("This appears to be 6.0.0_BETA1\n");
   } elsif ($startVersion eq "6.0.0_BETA2") {
@@ -2632,6 +2636,12 @@ sub upgrade5017GA {
       }
     }
   }
+}
+
+sub upgrade5018GA {
+  my ($startBuild, $targetVersion, $targetBuild) = (@_);
+  main::progress("Updating from 5.0.18_GA\n");
+  return 0;
 }
 
 sub upgrade600BETA1 {
