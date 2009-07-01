6a7eddf82e365146795a5039f940d0400f8e60e1
Unknown Perforce User <pfnguyen>
2009-07-01 14:55:20 -0500

    bug:38763
    LoggerSchema.pm: upgrade schema, add col_unit column, specify units for a number of counters
    
    ZaGlobalAdvancedStatsPage.js: support column units
    
    ZaGlobalMessageCount.js, ZaGlobalMessageVolume.js, ZaGlobalSpamActivityPage.js, ZaServerDiskStatsPage, ZaServerMessageCountPage.js, ZaServerMessageVolume.js, ZaServerSpamActivityPage.js: update to specify units when plotting charts
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=38763
    
    Copied from Perforce
     Change: 163976

--- lib/Zimbra/Mon/LoggerSchema.pm
+++ lib/Zimbra/Mon/LoggerSchema.pm
@@ -83,6 +83,87 @@ our @LOGGER_SCHEMA_UPGRADE = (
       	UPDATE config SET version = 2
       }
     ],
+    # version 3, counter types
+    [ q{
+    	ALTER table rrd_column_type ADD COLUMN col_unit varchar(64)
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = '% time', col_interval = 60
+      	 WHERE col_name IN ('gc_minor_ms', 'gc_major_ms')
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = 'times/second', col_interval = 60
+      	 WHERE col_name IN ('gc_minor_count', 'gc_major_count')
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = 'queries/s'
+      	 WHERE col_name = 'Slow_queries'
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = 'opens/s'
+      	 WHERE col_name = 'Opened_tables'
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = 'bytes'
+      	 WHERE col_name IN ('disk_use', 'disk_space')
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = 'events/s'
+      	 WHERE col_name IN ('clam_events', 'sendmail_events')
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = 'filter/s'
+      	 WHERE col_name IN ('filter_count', 'filter_misc', 'filter_virus', 'filter_spam')
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = 'msgs/s'
+      	 WHERE col_name = 'mta_count'
+      },
+      q{
+      	UPDATE rrd_column_type SET col_unit = 'bytes/s'
+      	 WHERE col_name = 'mta_volume'
+      },
+      q{
+      	INSERT INTO rrd_column_type select 'mysql.csv',  '*', 'G', 30, null
+      },
+      q{
+      	INSERT INTO rrd_column_type
+      	          select 'mtaqueue.csv', 'gc_minor_ms',       'G', 30, 'msgs'
+      	union all select 'fd.csv',       'fd_count',          'G', 30, 'fd'
+      	union all select 'cpu.csv',      '*',                 'G', 30, '% cpu'
+      	union all select 'vmstat.csv',   'pageins',           'G', 30, 'KB/s'
+      	union all select 'vmstat.csv',   'si',                'G', 30, 'KB/s'
+      	union all select 'vmstat.csv',   'pageout',           'G', 30, 'KB/s'
+      	union all select 'vmstat.csv',   'so',                'G', 30, 'KB/s'
+      	union all select 'vmstat.csv',   'free',              'G', 30, 'KB'
+      	union all select 'vmstat.csv',   'active',            'G', 30, 'KB'
+      	union all select 'vmstat.csv',   'inac',              'G', 30, 'KB'
+      	union all select 'vmstat.csv',   'Active',            'G', 30, 'KB'
+      	union all select 'vmstat.csv',   'Inactive',          'G', 30, 'KB'
+      	union all select 'vmstat.csv',   'cache',             'G', 30, 'KB'
+      	union all select 'vmstat.csv',   'cs',                'G', 30, 'cs/s'
+      	union all select 'vmstat.csv',   'r',                 'G', 30, 'run-q'
+      	union all select 'vmstat.csv',   'b',                 'G', 30, 'io-q'
+      	union all select 'vmstat.csv',   'loadavg',           'G', 30, null
+      	union all select 'soap.csv',     'exec_count',        'A', 30, 'calls/s'
+      	union all select 'soap.csv',     'exec_ms_avg',       'G', 30, null
+      	union all select 'mailboxd.csv', 'mbox_add_msg_count','A', 30, 'add/s'
+      	union all select 'mailboxd.csv', 'soap_count',        'A', 30, 'op/s'
+      	union all select 'mailboxd.csv', 'imap_count',        'A', 30, 'op/s'
+      	union all select 'mailboxd.csv', 'pop_count',         'A', 30, 'op/s'
+      	union all select 'mailboxd.csv', 'mbox_msg_cache',    'G', 30, 'cache hit %'
+      	union all select 'mailboxd.csv', 'mbox_item_cache',   'G', 30, 'cache hit %'
+      	union all select 'convertd.csv', 'cputime',           'G', 30, 'seconds/100'
+      	union all select 'convertd.csv', 'stime',             'G', 30, 'seconds/100'
+      	union all select 'convertd.csv', 'utime',             'G', 30, 'seconds/100'
+      	union all select 'convertd.csv', 'rss',               'G', 30, 'KB'
+      	union all select 'allprocs.csv', 'cputime',           'G', 30, 'seconds'
+      	union all select 'nginx.csv',    'cputime',           'G', 30, 'seconds/100'
+      	union all select 'nginx.csv',    'stime',             'G', 30, 'seconds/100'
+      	union all select 'nginx.csv',    'utime',             'G', 30, 'seconds/100'
+      	union all select 'nginx.csv',    'rss',               'G', 30, 'KB'
+      },
+    ],
 );
 
 1;
