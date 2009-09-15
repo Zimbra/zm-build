f2978ef4e1b05543a4cd9f2d7a9ca3f7c2e716f4
Quanah Gibson-Mount <quanah@zimbra.com>
2009-09-14 21:08:40 -0500

    bug: none Ability to upgrade to 5.0.20
    Copied from Perforce
     Change: 211068

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -184,6 +184,7 @@ my %updateFuncs = (
   "5.0.17_GA" => \&upgrade5017GA,
   "5.0.18_GA" => \&upgrade5018GA,
   "5.0.19_GA" => \&upgrade5019GA,
+  "5.0.20_GA" => \&upgrade5020GA,
   "6.0.0_BETA1" => \&upgrade600BETA1,
   "6.0.0_BETA2" => \&upgrade600BETA2,
   "6.0.0_RC1" => \&upgrade600RC1,
@@ -258,6 +259,7 @@ my @versionOrder = (
   "5.0.17_GA",
   "5.0.18_GA",
   "5.0.19_GA",
+  "5.0.20_GA",
   "6.0.0_BETA1",
   "6.0.0_BETA2",
   "6.0.0_RC1",
@@ -441,6 +443,8 @@ sub upgrade {
     main::progress("This appears to be 5.0.18_GA\n");
   } elsif ($startVersion eq "5.0.19_GA") {
     main::progress("This appears to be 5.0.19_GA\n");
+  } elsif ($startVersion eq "5.0.20_GA") {
+    main::progress("This appears to be 5.0.20_GA\n");
   } elsif ($startVersion eq "6.0.0_BETA1") {
     main::progress("This appears to be 6.0.0_BETA1\n");
   } elsif ($startVersion eq "6.0.0_BETA2") {
@@ -2659,6 +2663,12 @@ sub upgrade5018GA {
 sub upgrade5019GA {
   my ($startBuild, $targetVersion, $targetBuild) = (@_);
   main::progress("Updating from 5.0.19_GA\n");
+  return 0;
+}
+
+sub upgrade5020GA {
+  my ($startBuild, $targetVersion, $targetBuild) = (@_);
+  main::progress("Updating from 5.0.20_GA\n");
   return 0;
 }
 
