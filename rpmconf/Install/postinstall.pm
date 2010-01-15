964d99d987818ae385a0951f52d24f7ae6585c0a
Quanah Gibson-Mount <quanah@zimbra.com>
2010-01-14 21:15:45 -0600

    bug: none Update license preamble
    Copied from Perforce
     Change: 224396

--- rpmconf/Install/postinstall.pm
+++ rpmconf/Install/postinstall.pm
@@ -4,8 +4,8 @@
 # Zimbra Collaboration Suite Server
 # Copyright (C) 2005, 2006, 2007 Zimbra, Inc.
 # 
-# The contents of this file are subject to the Yahoo! Public License
-# Version 1.0 ("License"); you may not use this file except in
+# The contents of this file are subject to the Zimbra Public License
+# Version 1.2 ("License"); you may not use this file except in
 # compliance with the License.  You may obtain a copy of the License at
 # http://www.zimbra.com/license.
 # 
