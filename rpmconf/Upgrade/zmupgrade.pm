449ac6e3f89e3affbf97d0ab393076da148d1ec9
Quanah Gibson-Mount <quanah@zimbra.com>
2010-03-18 15:55:42 -0500

    bug: none main -> 6.0.7 Allow upgrades from 5.0.24
    Copied from Perforce
     Change: 229227

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -186,6 +186,7 @@ my %updateFuncs = (
   "5.0.21_GA" => \&upgrade5021GA,
   "5.0.22_GA" => \&upgrade5022GA,
   "5.0.23_GA" => \&upgrade5023GA,
+  "5.0.24_GA" => \&upgrade5024GA,
   "6.0.0_BETA1" => \&upgrade600BETA1,
   "6.0.0_BETA2" => \&upgrade600BETA2,
   "6.0.0_RC1" => \&upgrade600RC1,
@@ -197,6 +198,7 @@ my %updateFuncs = (
   "6.0.4_GA" => \&upgrade604GA,
   "6.0.5_GA" => \&upgrade605GA,
   "6.0.6_GA" => \&upgrade606GA,
+  "6.0.7_GA" => \&upgrade607GA,
 );
 
 my @versionOrder = (
@@ -268,6 +270,7 @@ my @versionOrder = (
   "5.0.21_GA",
   "5.0.22_GA",
   "5.0.23_GA",
+  "5.0.24_GA",
   "6.0.0_BETA1",
   "6.0.0_BETA2",
   "6.0.0_RC1",
@@ -279,6 +282,7 @@ my @versionOrder = (
   "6.0.4_GA",
   "6.0.5_GA",
   "6.0.6_GA",
+  "6.0.7_GA",
 );
 
 my ($startVersion,$startMajor,$startMinor,$startMicro);
@@ -463,6 +467,8 @@ sub upgrade {
     main::progress("This appears to be 5.0.22_GA\n");
   } elsif ($startVersion eq "5.0.23_GA") {
     main::progress("This appears to be 5.0.23_GA\n");
+  } elsif ($startVersion eq "5.0.24_GA") {
+    main::progress("This appears to be 5.0.24_GA\n");
   } elsif ($startVersion eq "6.0.0_BETA1") {
     main::progress("This appears to be 6.0.0_BETA1\n");
   } elsif ($startVersion eq "6.0.0_BETA2") {
@@ -485,6 +491,8 @@ sub upgrade {
     main::progress("This appears to be 6.0.5_GA\n");
   } elsif ($startVersion eq "6.0.6_GA") {
     main::progress("This appears to be 6.0.6_GA\n");
+  } elsif ($startVersion eq "6.0.7_GA") {
+    main::progress("This appears to be 6.0.7_GA\n");
   } else {
     main::progress("I can't upgrade version $startVersion\n\n");
     return 1;
@@ -2731,6 +2739,12 @@ sub upgrade5023GA {
   return 0;
 }
 
+sub upgrade5024GA {
+  my ($startBuild, $targetVersion, $targetBuild) = (@_);
+  main::progress("Updating from 5.0.24_GA\n");
+  return 0;
+}
+
 sub upgrade600BETA1 {
   my ($startBuild, $targetVersion, $targetBuild) = (@_);
   main::progress("Updating from 6.0.0_BETA1\n");
@@ -3142,6 +3156,12 @@ sub upgrade606GA {
     }
     $ldap->unbind;
   }
+  return 0;
+}
+
+sub upgrade607GA {
+  my ($startBuild, $targetVersion, $targetBuild) = (@_);
+  main::progress("Updating from 6.0.7_GA\n");
   return 0;
 }
 
