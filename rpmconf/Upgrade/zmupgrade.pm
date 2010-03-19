8fb575bcde6ad9216d8713948ef15a5bcbd18f8c
Quanah Gibson-Mount <quanah@zimbra.com>
2010-03-19 12:00:48 -0500

    bug: none Fix bad 5.0.0 upgrade step
    Copied from Perforce
     Change: 229328

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -2011,7 +2011,7 @@ sub upgrade500GA {
     main::runAsZimbra("$ZMPROV mcf zimbraMemcachedBindPort 11211");
 
     my $zimbraReverseProxyMailHostQuery = 
-      "\(\|\(zimbraMailDeliveryAddress=\${USER}\)\(zimbraMailAlias=\${USER}\)\)";
+      "\(\|\(zimbraMailDeliveryAddress=\${USER}\)\(zimbraMailAlias=\${USER}\)\(zimbraId=\${USER}\)\)";
     my $zimbraReverseProxyDomainNameQuery = 
       "\(\&\(zimbraVirtualIPAddress=\${IPADDR}\)\(objectClass=zimbraDomain\)\)";
     my $zimbraReverseProxyPortQuery = 
