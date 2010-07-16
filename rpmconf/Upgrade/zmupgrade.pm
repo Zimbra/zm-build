baeb68e28096020a5623aa683dcb16e6046fea76
Quanah Gibson-Mount <quanah@zimbra.com>
2010-07-16 13:39:35 -0500

    Integrated the following from //depot/main/...: Change 243065 by quanah@qmain on 2010/07/16 11:39:20
    bug: 48131
    Fix sed statement for OSX
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=48131
    
    Copied from Perforce
     Change: 243066

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -202,7 +202,6 @@ my %updateFuncs = (
   "6.0.7_GA" => \&upgrade607GA,
   "6.0.8_GA" => \&upgrade608GA,
   "6.0.9_GA" => \&upgrade609GA,
-  "7.0.0_BETA1" => \&upgrade700BETA1,
 );
 
 my @versionOrder = (
@@ -290,7 +289,6 @@ my @versionOrder = (
   "6.0.7_GA",
   "6.0.8_GA",
   "6.0.9_GA",
-  "7.0.0_BETA1",
 );
 
 my ($startVersion,$startMajor,$startMinor,$startMicro);
@@ -507,8 +505,6 @@ sub upgrade {
     main::progress("This appears to be 6.0.8_GA\n");
   } elsif ($startVersion eq "6.0.9_GA") {
     main::progress("This appears to be 6.0.9_GA\n");
-  } elsif ($startVersion eq "7.0.0_BETA1") {
-    main::progress("This appears to be 7.0.0_BETA1\n");
   } else {
     main::progress("I can't upgrade version $startVersion\n\n");
     return 1;
