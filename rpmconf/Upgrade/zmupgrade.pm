dff7920747c43ab0bfc4e4bc504745349f662a97
Quanah Gibson-Mount <quanah@zimbra.com>
2010-11-10 15:21:52 -0600

    bug: none GNR -> 6.0.10
    Copied from Perforce
     Change: 257321

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -202,6 +202,7 @@ my %updateFuncs = (
   "6.0.7_GA" => \&upgrade607GA,
   "6.0.8_GA" => \&upgrade608GA,
   "6.0.9_GA" => \&upgrade609GA,
+  "6.0.10_GA" => \&upgrade6010GA,
   "7.0.0_RC1" => \&upgrade700RC1,
 );
 
@@ -290,6 +291,7 @@ my @versionOrder = (
   "6.0.7_GA",
   "6.0.8_GA",
   "6.0.9_GA",
+  "6.0.10_GA",
   "7.0.0_RC1",
 );
 
@@ -507,6 +509,8 @@ sub upgrade {
     main::progress("This appears to be 6.0.8_GA\n");
   } elsif ($startVersion eq "6.0.9_GA") {
     main::progress("This appears to be 6.0.9_GA\n");
+  } elsif ($startVersion eq "6.0.10_GA") {
+    main::progress("This appears to be 6.0.10_GA\n");
   } elsif ($startVersion eq "7.0.0_RC1") {
     main::progress("This appears to be 7.0.0_RC1\n");
   } else {
@@ -3274,6 +3278,12 @@ sub upgrade608GA {
 sub upgrade609GA {
   my ($startBuild, $targetVersion, $targetBuild) = (@_);
   main::progress("Updating from 6.0.9_GA\n");
+  return 0;
+}
+
+sub upgrade6010GA {
+  my ($startBuild, $targetVersion, $targetBuild) = (@_);
+  main::progress("Updating from 6.0.10_GA\n");
   if (main::isInstalled("zimbra-ldap")) {
     runLdapAttributeUpgrade("50458");
   }
