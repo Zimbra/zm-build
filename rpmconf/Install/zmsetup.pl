82d20b1f6229050f9d77292d0be8f062bbc8f09a
Quanah Gibson-Mount <quanah@zimbra.com>
2011-08-05 14:26:04 -0500

    bug: 63031 Fix missing }
    http://bugzilla.zimbra.com/show_bug.cgi?id=63031
    
    Copied from Perforce
     Change: 324343

--- rpmconf/Install/zmsetup.pl
+++ rpmconf/Install/zmsetup.pl
@@ -1329,7 +1329,7 @@ sub setDefaults {
   }
   if ($platform =~ /MACOSX/ && $platform ne "MACOSXx86_10.6" && $platform ne "MACOSXx86_10.7" ) {
       $config{mailboxd_truststore} = "/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Home/lib/security/cacerts";
-  elsif ($platform eq "MACOSXx86_10.6" || $platform eq "MACOSXx86_10.7") {
+  } elsif ($platform eq "MACOSXx86_10.6" || $platform eq "MACOSXx86_10.7") {
       $config{mailboxd_truststore} = "$config{JAVAHOME}/lib/security/cacerts";
   } else {
     if ( -f "/opt/zimbra/java/lib/security/cacerts") {
