a5a45dded06c4bd5d10473d17dd3d8e4eff6ab06
Unknown Perforce User <smukhopadhyay>
2012-02-29 18:39:34 -0600

    Bug: 45932 Drop IM tables from D/B.
    https://reviewboard.eng.vmware.com/r/311656/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=45932
    
    Copied from Perforce
     Change: 350754

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -37,7 +37,7 @@ chomp $rundir;
 my $scriptDir = "/opt/zimbra/libexec/scripts";
 
 my $lowVersion = 18;
-my $hiVersion = 88; # this should be set to the DB version expected by current server code
+my $hiVersion = 89; # this should be set to the DB version expected by current server code
 
 # Variables for the combo schema updater
 my $comboLowVersion = 20;
@@ -123,6 +123,7 @@ my %updateScripts = (
   '85' => "migrate20110929-VersionColumn.pl",          # 8.0.0_BETA2
   '86' => "migrate20120125-uuidAndDigest.pl",          # 8.0.0_BETA2
   '87' => "migrate20120222-LastPurgeAtColumn.pl",      # 8.0.0_BETA2
+  '88' => "migrate20120229-DropIMTables.pl",           # 8.0.0_BETA2
 );
 
 my %updateFuncs = (
