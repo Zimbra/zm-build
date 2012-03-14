b6aaa9ef1f978620731c0bb6188f1cb4d8a49c10
Quanah Gibson-Mount <quanah@zimbra.com>
2012-03-14 18:49:12 -0500

    bug: 70064 zmbes password only matters on the master
    http://bugzilla.zimbra.com/show_bug.cgi?id=70064
    
    Copied from Perforce
     Change: 353151

--- rpmconf/Install/zmsetup.pl
+++ rpmconf/Install/zmsetup.pl
@@ -1431,7 +1431,9 @@ sub setDefaults {
     $ldapPostChanged = 1;
     $ldapAmavisChanged = 1;
     $ldapNginxChanged = 1;
-    $ldapBesSearcherChanged = 1;
+    if (isLdapMaster()) {
+      $ldapBesSearcherChanged = 1;
+    }
   }
 
   if(isInstalled("zimbra-proxy") && !isEnabled("zimbra-ldap")) {
@@ -1815,10 +1817,12 @@ sub setDefaultsFromLocalConfig {
     }
   } 
   if (isEnabled("zimbra-ldap")) {
-    $config{ldap_bes_searcher_password} = getLocalConfig ("ldap_bes_searcher_password");
-    if ($config{ldap_bes_searcher_password} eq "") {
-      $config{ldap_bes_searcher_password} = $config{LDAPADMINPASS};
-      $ldapBesSearcherChanged = 1;
+    if (isLdapMaster()) {
+      $config{ldap_bes_searcher_password} = getLocalConfig ("ldap_bes_searcher_password");
+      if ($config{ldap_bes_searcher_password} eq "") {
+        $config{ldap_bes_searcher_password} = $config{LDAPADMINPASS};
+        $ldapBesSearcherChanged = 1;
+      }
     }
   } 
   if (isEnabled("zimbra-ldap") || isEnabled("zimbra-mta")) {
