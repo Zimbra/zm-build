98b60fd4f93397bcf68a671470fb7195e1ce1482
Unknown Perforce User <bpeterson>
2012-05-01 10:32:45 -0500

    bug: 73652
    http://bugzilla.zimbra.com/show_bug.cgi?id=73652
    
    Copied from Perforce
     Change: 366967

--- lib/Zimbra/Util/Common.pm
+++ lib/Zimbra/Util/Common.pm
@@ -22,8 +22,8 @@ use lib "/opt/zimbra/zimbramon/lib";
 use lib "/opt/zimbra/zimbramon/lib/Zimbra/SOAP";
 use lib "/opt/zimbra/zimbramon/lib/Zimbra/Mon";
 use lib "/opt/zimbra/zimbramon/lib/Zimbra/DB";
-foreach my $arch qw(i386 x86_64 i486 i586 i686 darwin) {
-  foreach my $type qw(linux-thread-multi linux-gnu-thread-multi linux thread-multi thread-multi-2level) {
+foreach my $arch (qw(i386 x86_64 i486 i586 i686 darwin)) {
+  foreach my $type (qw(linux-thread-multi linux-gnu-thread-multi linux thread-multi thread-multi-2level)) {
     my $dir = "/opt/zimbra/zimbramon/lib/${arch}-${type}";
     unshift(@INC, "$dir") 
       if (-d "$dir");
