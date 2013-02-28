95a57484059913398fd6aeb13d7b3c2158c6eb0b
Quanah Gibson-Mount <quanah@zimbra.com>
2013-02-28 16:11:01 -0600

    bug: 80577 Run mysql_upgrade when going from < 9.0.0_BETA1 -> 9.0.0_BETA1 or later
    http://bugzilla.zimbra.com/show_bug.cgi?id=80577
    
    Copied from Perforce
     Change: 449657

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -687,6 +687,7 @@ sub upgrade {
       $needMysqlTableCheck=1 if ($v eq "4.5.2_GA");
       $needMysqlUpgrade=1 if ($v eq "7.0.0_BETA1");
       $needMysqlUpgrade=1 if ($v eq "8.0.0_GA");
+      $needMysqlUpgrade=1 if ($v eq "9.0.0_BETA1");
     }
     last if ($v eq $targetVersion);
   }
