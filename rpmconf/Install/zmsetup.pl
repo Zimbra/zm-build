eb89ff44b48fe4c18e0fb5d81cfbd22852f1d799
Quanah Gibson-Mount <quanah@zimbra.com>
2013-04-09 13:03:19 -0500

    bug: 81498 zmiptool does this now, no need to hand set it here too
    http://bugzilla.zimbra.com/show_bug.cgi?id=81498
    
    Copied from Perforce
     Change: 461476

--- rpmconf/Install/zmsetup.pl
+++ rpmconf/Install/zmsetup.pl
@@ -1350,12 +1350,8 @@ sub setDefaults {
 
   if (!$ipv4found && $ipv6found) {
     $config{zimbraIPMode}     = "ipv6";
-    setLocalConfig ("antispam_mysql_host", "::1");
-    setLocalConfig ("mysql_bind_address", "::1");
   } else {
     $config{zimbraIPMode}     = "ipv4";
-    setLocalConfig ("antispam_mysql_host", "127.0.0.1");
-    setLocalConfig ("mysql_bind_address", "127.0.0.1");
   }
 
   if ($platform =~ /MACOSX/ && $platform ne "MACOSXx86_10.6" && $platform ne "MACOSXx86_10.7" ) {
@@ -3133,13 +3129,6 @@ sub setIPMode {
     if ($new eq "ipv4" ||  $new eq "both" || $new eq "ipv6") {
       if ($config{zimbraIPMode} ne $new) {
         $config{zimbraIPMode} = $new;
-      }
-      if ($new eq "ipv4") {
-        setLocalConfig ("antispam_mysql_host", "127.0.0.1");
-        setLocalConfig ("mysql_bind_address", "127.0.0.1");
-      } else {
-        setLocalConfig ("antispam_mysql_host", "::1");
-        setLocalConfig ("mysql_bind_address", "::1");
       }
       return;
     } else {
