8d3fb872abf5077bf3965630cf7033d6942ce480
Quanah Gibson-Mount <quanah@zimbra.com>
2013-11-07 13:28:41 -0600

    bug: none 7.2.7 upgrade step
    Copied from Perforce
     Change: 490356

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -231,6 +231,7 @@ my %updateFuncs = (
   "7.2.4_GA" => \&upgrade724GA,
   "7.2.5_GA" => \&upgrade725GA,
   "7.2.6_GA" => \&upgrade726GA,
+  "7.2.7_GA" => \&upgrade727GA,
   "8.0.0_BETA1" => \&upgrade800BETA1,
 );
 
@@ -346,6 +347,7 @@ my @versionOrder = (
   "7.2.4_GA",
   "7.2.5_GA",
   "7.2.6_GA",
+  "7.2.7_GA",
   "8.0.0_BETA1",
 );
 
@@ -633,6 +635,8 @@ sub upgrade {
     main::progress("This appears to be 7.2.5_GA\n");
   } elsif ($startVersion eq "7.2.6_GA") {
     main::progress("This appears to be 7.2.6_GA\n");
+  } elsif ($startVersion eq "7.2.7_GA") {
+    main::progress("This appears to be 7.2.7_GA\n");
   } elsif ($startVersion eq "8.0.0_BETA1") {
     main::progress("This appears to be 8.0.0_BETA1\n");
   } else {
@@ -4023,6 +4027,12 @@ sub upgrade725GA {
 sub upgrade726GA {
   my ($startBuild, $targetVersion, $targetBuild) = (@_);
   main::progress("Updating from 7.2.6_GA\n");
+  return 0;
+}
+
+sub upgrade727GA {
+  my ($startBuild, $targetVersion, $targetBuild) = (@_);
+  main::progress("Updating from 7.2.7_GA\n");
   return 0;
 }
 
