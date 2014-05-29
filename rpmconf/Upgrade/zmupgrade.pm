056afe09f2ddc383c8130de18b68b304889676f6
Unknown Perforce User <kpariani>
2014-05-29 14:23:36 -0500

    Bug 85224 - SSL Forward Secrecy configuration for ZCS Adding the upgrade step in java to update the default value of zimbraReverseProxySSLCiphers on a 8.0 -> 8.5 upgrade
    http://bugzilla.zimbra.com/show_bug.cgi?id=85224
    
    Copied from Perforce
     Change: 512914

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -2620,6 +2620,11 @@ sub upgrade850BETA2 {
 sub upgrade850BETA3 {
     my ($startBuild, $targetVersion, $targetBuild) = (@_);
     main::progress("Updating from 8.5.0_BETA3\n");
+    if (main::isInstalled("zimbra-ldap")) {
+      if ($isLdapMaster) {
+        runLdapAttributeUpgrade("85224");
+      }
+    }
     return 0;
 }
 
