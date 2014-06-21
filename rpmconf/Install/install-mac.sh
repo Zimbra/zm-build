a5c8e456c8651120953fdffe800cba962ff0d11d
Quanah Gibson-Mount <quanah@zimbra.com>
2014-06-20 23:59:29 -0500

    bug: 84594 relicense main
    http://bugzilla.zimbra.com/show_bug.cgi?id=84594
    
    Copied from Perforce
     Change: 515096

--- rpmconf/Install/install-mac.sh
+++ rpmconf/Install/install-mac.sh
@@ -2,15 +2,17 @@
 # 
 # ***** BEGIN LICENSE BLOCK *****
 # Zimbra Collaboration Suite Server
-# Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2013 Zimbra Software, LLC.
+# Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2013 Zimbra, Inc.
 # 
-# The contents of this file are subject to the Zimbra Public License
-# Version 1.4 ("License"); you may not use this file except in
-# compliance with the License.  You may obtain a copy of the License at
-# http://www.zimbra.com/license.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software Foundation,
+# version 2 of the License.
 # 
-# Software distributed under the License is distributed on an "AS IS"
-# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
 # ***** END LICENSE BLOCK *****
 # 
 
