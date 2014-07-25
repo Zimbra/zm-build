adb4fd720eaf72944804e5cf55f638f4b41b95dc
Quanah Gibson-Mount <quanah@zimbra.com>
2014-07-25 18:44:01 -0500

    Integrated the following from //depot/zimbra/main/...: Change 518449 by quanah@qmain on 2014/07/22 11:28:49
    bug: 93952
    Fix CentOS7 detection
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=93952
    
    Copied from Perforce
     Change: 519026

--- rpmconf/Build/get_plat_tag.sh
+++ rpmconf/Build/get_plat_tag.sh
@@ -86,7 +86,7 @@ if [ -f /etc/redhat-release ]; then
 		exit 0
 	fi
 
-	grep "CentOS release 7" /etc/redhat-release > /dev/null 2>&1
+	grep "CentOS Linux release 7" /etc/redhat-release > /dev/null 2>&1
 	if [ $? = 0 ]; then
 		echo "RHEL7${i}"
 		exit 0
