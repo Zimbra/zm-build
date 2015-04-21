d347a93cd27434610a57f6dc43482a27e33c845f
Rohan Ambasta <rambasta@zimbra.com>
2015-04-21 04:56:48 -0500

    Bug: 98884 - Restore does not work after upgrade due to missing ZMG tables
    Looks like runSchemaUpgrade is not getting called because $curSchemaVersion < $hiVersion is false
    
    1. JP/zmupgrade.pm
       updated value of my $hiVersion = 104;
    
    2. main/zmupgrade.pm
       updated value of my $hiVersion = 121;
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=98884
    
    Copied from Perforce
     Change: 542649

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -41,7 +41,7 @@ chomp $rundir;
 my $scriptDir = "/opt/zimbra/libexec/scripts";
 
 my $lowVersion = 52;
-my $hiVersion = 103; # this should be set to the DB version expected by current server code
+my $hiVersion = 104; # this should be set to the DB version expected by current server code
 
 my $needSlapIndexing = 0;
 my $mysqlcnfUpdated = 0;
