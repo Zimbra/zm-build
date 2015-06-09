fe2c493081038d65fd5cac0c8e4eb99fab319ca7
Ilya Raykin <raykini@zimbra.com>
2015-06-09 17:50:08 -0500

    Bug: 97783
    updated $hiVersion in zmupgrade.pm to 105
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97783
    
    Copied from Perforce
     Change: 547029

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -41,7 +41,7 @@ chomp $rundir;
 my $scriptDir = "/opt/zimbra/libexec/scripts";
 
 my $lowVersion = 52;
-my $hiVersion = 104; # this should be set to the DB version expected by current server code
+my $hiVersion = 105; # this should be set to the DB version expected by current server code
 
 my $needSlapIndexing = 0;
 my $mysqlcnfUpdated = 0;
