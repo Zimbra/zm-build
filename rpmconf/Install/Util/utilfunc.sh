749ff79d71ca0d1be8415dd4614586f67407005f
Quanah Gibson-Mount <quanah@zimbra.com>
2015-11-17 23:57:32 -0600

    bug: 102322 Fix upgrade logic for releases that don't have a native memcached install
    https://bugzilla.zimbra.com/show_bug.cgi?id=102322
    
    Copied from Perforce
     Change: 559879

--- rpmconf/Install/Util/utilfunc.sh
+++ rpmconf/Install/Util/utilfunc.sh
@@ -1679,7 +1679,12 @@ removeExistingInstall() {
       if [ x$PKGINSTALLED != "x" ]; then
         echo -n "   $p..."
 	if [ x$p = "xzimbra-memcached" ]; then
-          $REPORM zimbra-memcached-base >>$LOGFILE 2>&1
+          isInstalled "zimbra-memcached-base"
+          if [ x$PKGINSTALLED != "x" ]; then
+            $REPORM zimbra-memcached-base >>$LOGFILE 2>&1
+          else
+            $PACKAGERM $p > /dev/null 2>&1
+          fi
 	else
           $PACKAGERM $p > /dev/null 2>&1
 	fi
