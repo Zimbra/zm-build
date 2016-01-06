2edb8d49e2382a0d649ea32d10140c1aac6ade34
Quanah Gibson-Mount <quanah@zimbra.com>
2016-01-05 22:49:31 -0600

    bug: 103128 Go back to old method, as using $releasever is not safe
    https://bugzilla.zimbra.com/show_bug.cgi?id=103128
    
    Copied from Perforce
     Change: 562514

--- rpmconf/Install/Util/utilfunc.sh
+++ rpmconf/Install/Util/utilfunc.sh
@@ -2053,7 +2053,11 @@ EOF
         exit 1
       fi
     else
-      if [ x$PLATFORM != "xRHEL6_64" -a x$PLATFORM != "xRHEL7_64" ]; then
+      if [ $PLATFORM = "RHEL6_64" ]; then
+        repo="rhel6"
+      elif [ $PLATFORM = "RHEL7_64" ]; then
+        repo="rhel7"
+      else
         print "Aborting, unknown platform: $PLATFORM"
         exit 1
       fi
@@ -2067,7 +2071,7 @@ EOF
 cat > /etc/yum.repos.d/zimbra.repo <<EOF
 [zimbra]
 name=Zimbra RPM Repository
-baseurl=https://$PACKAGE_SERVER/rpm/90/rhel\$releasever
+baseurl=https://$PACKAGE_SERVER/rpm/90/$repo
 gpgcheck=1
 enabled=1
 EOF
