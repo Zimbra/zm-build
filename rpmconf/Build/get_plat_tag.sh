bb0de091baeb69aa3382eb897f4c353530f5dbed
Quanah Gibson-Mount <quanah@zimbra.com>
2016-01-11 22:38:24 -0600

    bug: 103161 Update get_plat_tag.sh for newer OSes to help encourage community support for Debian, Fedora, etc Make platform detection on debian/ubuntu smarter
    https://bugzilla.zimbra.com/show_bug.cgi?id=103161
    
    Copied from Perforce
     Change: 562910

--- rpmconf/Build/get_plat_tag.sh
+++ rpmconf/Build/get_plat_tag.sh
@@ -1,156 +1,195 @@
 #!/bin/bash
-# 
+#
 # ***** BEGIN LICENSE BLOCK *****
 # Zimbra Collaboration Suite Server
 # Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
-# 
+#
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software Foundation,
 # version 2 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program.
 # If not, see <http://www.gnu.org/licenses/>.
 # ***** END LICENSE BLOCK *****
-# 
+#
 
 
 if [ -f /etc/redhat-release ]; then
+   i=`uname -i`
+   if [[ "x$i" == "xx86_64" ]] || [[ "x$i" == "xppc64"* ]]; then
+        i="_64"
+  else
+    i=""
+  fi
 
-	i=`uname -i`
-	if [ "x$i" = "xx86_64" ]; then
-		i="_64"
-	else 
-		i=""
-	fi
+  grep "Red Hat Enterprise Linux.*release 7" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL7${i}"
+    exit 0
+  fi
+  grep "Red Hat Enterprise Linux.*release 6" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL6${i}"
+    exit 0
+  fi
 
-	grep "Red Hat Enterprise Linux.*release 7" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "RHEL7${i}"
-		exit 0
-	fi
-	grep "Red Hat Enterprise Linux.*release 6" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "RHEL6${i}"
-		exit 0
-	fi
+  grep "CentOS Linux release 7" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL7${i}"
+    exit 0
+  fi
+  grep "CentOS release 6" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL6${i}"
+    exit 0
+  fi
 
-	grep "CentOS Linux release 7" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "RHEL7${i}"
-		exit 0
-	fi
-	grep "CentOS release 6" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "RHEL6${i}"
-		exit 0
-	fi
+  grep "Scientific Linux release 7" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL7${i}"
+    exit 0
+  fi
+  grep "Scientific Linux release 6" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL6${i}"
+    exit 0
+  fi
 
-	grep "Scientific Linux release 7" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "RHEL7${i}"
-		exit 0
-	fi
-	
-	grep "Scientific Linux release 6" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "RHEL6${i}"
-		exit 0
-	fi
-	
-	grep "Red Hat Enterprise Linux.*release" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "RHELUNKNOWN${i}"
-		exit 0
-	fi
-	grep "CentOS release" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "CentOSUNKNOWN${i}"
-		exit 0
-	fi
-	grep "CentOS Linux release" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "CentOSUNKNOWN${i}"
-		exit 0
-	fi
-	grep "Fedora Core release" /etc/redhat-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "FCUNKNOWN${i}"
-		exit 0
-	fi
+  grep "Fedora release 23" /etc/redhat-release >/dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "F23${i}"
+    exit 0
+  fi
+  grep "Fedora release 22" /etc/redhat-release >/dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "F22${i}"
+    exit 0
+  fi
+
+  grep "Red Hat Enterprise Linux.*release" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHELUNKNOWN${i}"
+    exit 0
+  fi
+  grep "CentOS release" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "CentOSUNKNOWN${i}"
+    exit 0
+  fi
+  grep "Fedora Core release" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "FCUNKNOWN${i}"
+    exit 0
+  fi
 fi
 
 if [ -f /etc/SuSE-release ]; then
+  i=`uname -i`
+   if [[ "x$i" == "xx86_64" ]] || [[ "x$i" == "xppc64"* ]]; then
+    i="_64"
+  else
+    i=""
+  fi
 
-	grep "SUSE Linux Enterprise Server" /etc/SuSE-release >/dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "SLESUNKNOWN${i}"
-		exit 0
-	fi
-	grep "openSUSE" /etc/SuSE-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo "openSUSEUNKNOWN${i}"
-		exit 0
-	fi
-fi
-
-if [ -f /etc/debian_version ]; then
-	if [ ! -f /etc/lsb-release ]; then
-		i=`dpkg --print-architecture`
-		if [ "x$i" = "xamd64" ]; then
-			i="_64"
-		else 
-			i=""
-		fi
-	        echo "DEBIANUNKNOWN${i}"
-	       	exit 0
-	fi
+  grep "SUSE Linux Enterprise Server 11" /etc/SuSE-release >/dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "SLES11${i}"
+    exit 0
+  fi
+  grep "SUSE Linux Enterprise Server 10" /etc/SuSE-release >/dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "SLES10${i}"
+    exit 0
+  fi
+  grep "SUSE Linux Enterprise Server" /etc/SuSE-release >/dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "SLESUNKNOWN${i}"
+    exit 0
+  fi
+  grep "openSUSE" /etc/SuSE-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "openSUSEUNKNOWN${i}"
+    exit 0
+  fi
 fi
 
 if [ -f /etc/lsb-release ]; then
-	i=`dpkg --print-architecture`
-	if [ "x$i" = "xamd64" ]; then
-		i="_64"
-	else 
-		i=""
-	fi
-	grep "DISTRIB_ID=Ubuntu" /etc/lsb-release > /dev/null 2>&1
-	if [ $? = 0 ]; then
-		echo -n "UBUNTU"
-		grep "DISTRIB_RELEASE=12" /etc/lsb-release > /dev/null 2>&1
-		if [ $? = 0 ]; then
-			echo "12${i}"
-			exit 0
-		fi
-		grep "DISTRIB_RELEASE=14" /etc/lsb-release > /dev/null 2>&1
-		if [ $? = 0 ]; then
-			echo "14${i}"
-			exit 0
-		else
-			echo "UNKNOWN${i}"
-			exit 0
-		fi
-	fi
-	echo "DEBIANUNKNOWN${i}"
-	exit 0
+  LSB="lsb_release"
+  i=`dpkg --print-architecture`
+  if [ "x$i" = "xamd64" ]; then
+    i="_64"
+  else
+    i=""
+  fi
+  RELEASE=$($LSB -s -c)
+  DISTRIBUTOR=$($LSB -s -i)
+  if [ "$DISTRIBUTOR" = "Ubuntu" ]; then
+    echo -n "UBUNTU"
+    if [ "$RELEASE" = "precise" ]; then
+      echo "12${i}"
+      exit 0
+    fi
+    if [ "$RELEASE" = "trusty" ]; then
+      echo "14${i}"
+      exit 0
+    else
+      echo "UNKNOWN${i}"
+      exit 0
+    fi
+  fi
+  if [ "$DISTRIBUTOR" = "Debian" ]; then
+    echo -n "DEBIAN"
+    if [ "$RELEASE" = "wheezy" ]; then
+      echo "7${i}"
+      exit 0
+    fi
+    if [ "$RELEASE" = "jesse" ]; then
+      echo "8${i}"
+      exit 0
+    else
+      echo "UNKNOWN${i}"
+      exit 0
+    fi
+  fi
+  echo "DEBIANUNKNOWN${i}"
+  exit 0
+fi
+
+if [ -f /etc/debian_version ]; then
+  echo "DEBIANUNKNOWN${i}"
+  exit 0
+fi
+
+if [ -f /etc/mandriva-release ]; then
+  echo "MANDRIVAUNKNOWN"
+  exit 0
+fi
+
+if [ -f /etc/release ]; then
+  egrep 'Solaris 10.*X86' /etc/release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "SOLARISX86"
+    exit 0
+  fi
+  echo "SOLARISUNKNOWN"
 fi
 
 a=`uname -a | awk '{print $1}'`
 p=`uname -p`
-
 if [ "x$a" = "xDarwin" ]; then
   v=`sw_vers | grep ^ProductVersion | awk '{print $NF}' | awk -F. '{print $1"."$2}'`
   if [ "x$v" = "x10.4" ]; then
-	  if [ "x$p" = "xi386" ]; then
-		  echo "MACOSXx86"
-		  exit 0
-	  fi
+    if [ "x$p" = "xi386" ]; then
+      echo "MACOSXx86"
+      exit 0
+    fi
 
     if [ "x$p" = "xpowerpc" ]; then
-	    echo "MACOSX"
-	    exit 0
+      echo "MACOSX"
+      exit 0
     fi
   else
     if [ "x$p" = "xi386" ]; then
