dce581d1976bcec771802dc33cbacffdd49e4545
Sakchhi Kiran <sakchhi.kiran@synacor.com>
2016-11-18 07:36:39 -0500

    Removed unnecessary mkdir line and changed formating.

--- scripts/packages/zimbra-logger.sh
+++ scripts/packages/zimbra-logger.sh
@@ -15,25 +15,26 @@
 # If not, see <https://www.gnu.org/licenses/>.
 # ***** END LICENSE BLOCK *****
 
+
 #-------------------- Configuration ---------------------------
 
         currentScript=`basename $0 | cut -d "." -f 1`
         currentPackage=`echo ${currentScript}build | cut -d "-" -f 2`
 
-#-------------------- Package Script ---------------------------
 
-        echo -e "\n\tCreate build directories...\n" >> ${buildLogFile}
-        mkdir -p ${repoDir}/zm-build/${currentPackage}/opt/zimbra/logger/db
-        mkdir -p ${repoDir}/zm-build/${currentPackage}/opt/zimbra/logger/db/data
-        mkdir -p ${repoDir}/zm-build/${currentPackage}/DEBIAN
+#-------------------- Build Package ---------------------------
 
-        echo -e "\tCopy build files...\n" >> ${buildLogFile}
+	echo -e "\tCreate build directories..." >> ${buildLogFile}
+	mkdir -p ${repoDir}/zm-build/${currentPackage}/opt/zimbra/logger/db/data
+	mkdir -p ${repoDir}/zm-build/${currentPackage}/DEBIAN
+
+	echo -e "\tCopy build files..." >> ${buildLogFile}
 	cat ${repoDir}/zm-build/rpmconf/Spec/Scripts/zimbra-logger.post >> ${repoDir}/zm-build/${currentPackage}/DEBIAN/postinst
-        chmod 555 ${repoDir}/zm-build/${currentPackage}/DEBIAN/*
- 	cat ${repoDir}/zm-build/rpmconf/Spec/zimbra-logger.deb | sed -e "s/@@VERSION@@/${release}.${buildno}.${os/_/.}/" -e "s/@@branch@@/${buildTimeStamp}/" -e "s/@@ARCH@@/${arch}/" > ${repoDir}/zm-build/${currentPackage}/DEBIAN/control
+	chmod 555 ${repoDir}/zm-build/${currentPackage}/DEBIAN/*
+	cat ${repoDir}/zm-build/rpmconf/Spec/zimbra-logger.deb | sed -e "s/@@VERSION@@/${release}.${buildno}.${os/_/.}/" -e "s/@@branch@@/${buildTimeStamp}/" -e "s/@@ARCH@@/${arch}/" > ${repoDir}/zm-build/${currentPackage}/DEBIAN/control
 
-        echo -e "\tCreate debian package...\n" >> ${buildLogFile}
-        (cd ${repoDir}/zm-build/${currentPackage}; dpkg -b ${repoDir}/zm-build/${currentPackage} ${repoDir}/zm-build/${arch})
+	echo -e "\tCreate debian package..." >> ${buildLogFile}
+	(cd ${repoDir}/zm-build/${currentPackage}; dpkg -b ${repoDir}/zm-build/${currentPackage} ${repoDir}/zm-build/${arch})
 
 	if [ $? -ne 0 ]; then
 		echo -e "\t### ${currentPackage} package building failed ###" >> ${buildLogFile}
