8c2bc85f5935c0294d4a4b062a35f8a3acfe7515
Tarang Khandelwal <tkhandelwal@zimbra.com>
2016-12-27 11:46:29 +0530

    Removing steps to copy templates files in zimbraAdmin. These are not required at this location and are part of zimbra app.

--- scripts/packages/zimbra-store.sh
+++ scripts/packages/zimbra-store.sh
@@ -192,9 +192,6 @@
         cp ${repoDir}/zm-build/${currentPackage}/opt/zimbra/${jettyVersion}/webapps/zimbra/public/${i} ${repoDir}/zm-build/${currentPackage}/opt/zimbra/${jettyVersion}/webapps/zimbraAdmin/public
     done
 
-    echo "\t\t***** templates content *****" >> ${buildLogFile}
-    cp -rf ${repoDir}/zm-build/${currentPackage}/opt/zimbra/${jettyVersion}/webapps/zimbra/templates/abook ${repoDir}/zm-build/${currentPackage}/opt/zimbra/${jettyVersion}/webapps/zimbraAdmin/templates
-    cp -rf ${repoDir}/zm-build/${currentPackage}/opt/zimbra/${jettyVersion}/webapps/zimbra/templates/calendar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/${jettyVersion}/webapps/zimbraAdmin/templates
 
     echo "\t\t***** messages content *****" >> ${buildLogFile}
     declare -a messagesArray=("ZbMsg*.properties" "ZhMsg*.properties" "ZmMsg*.properties" "ZMsg*.properties" "ZmSMS*.properties" "ZtMsg*.properties" "AjxTemplateMsg*.properties")
