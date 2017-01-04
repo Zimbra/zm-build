623af4338aa66e8017d5abfbb11b3b5089f4dd1f
Rupali Desai <rupali.desai@synacor.com>
2017-01-04 15:29:52 +0530

    Fixed missing convertd ldif file from ldap pkg

--- scripts/packages/zimbra-ldap.sh
+++ scripts/packages/zimbra-ldap.sh
@@ -36,6 +36,7 @@ main()
 
     echo -e "\tCopy package files" >> ${buildLogFile}
     cp -rf ${ldapSchemaDir}/*  ${repoDir}/zm-build/${currentPackage}/opt/zimbra/common/etc/openldap/zimbra
+    cp -f ${repoDir}/zm-convertd-native/conf/ldap/zimbra_mimehandlers.ldif ${repoDir}/zm-build/${currentPackage}/opt/zimbra/common/etc/openldap/zimbra/convertd_mimehandlers.ldif
     cp ${repoDir}/zm-build/rpmconf/Env/sudoers.d/02_${currentScript} ${repoDir}/zm-build/${currentPackage}/etc/sudoers.d/
 
     CreateDebianPackage
