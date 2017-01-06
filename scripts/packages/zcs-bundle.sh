e5f792005bb1bacbe95bec395090bea40e196b40
Shriram V <shriram.vishwanathan@synacor.com>
2017-01-06 19:25:04 +0530

    ZCS-446 - Fixed the names of RHEL packages

--- scripts/packages/zcs-bundle.sh
+++ scripts/packages/zcs-bundle.sh
@@ -17,56 +17,56 @@
 
 cd ${repoDir}/zm-build
 
-mkdir -p zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin
-mkdir -p zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/data
-mkdir -p zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/docs/en_US
-mkdir -p zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/lib/jars
-mkdir -p zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/packages
-mkdir -p zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/util/modules
+mkdir -p zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin
+mkdir -p zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/data
+mkdir -p zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/docs/en_US
+mkdir -p zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/lib/jars
+mkdir -p zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/packages
+mkdir -p zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/util/modules
 
-cp -f ${repoDir}/zm-build/RE/README.txt                                                 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/
-cp -f ${repoDir}/zm-build/rpmconf/Build/checkLicense.pl                                 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin
-cp -f ${repoDir}/zm-build/rpmconf/Build/checkService.pl                                 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin
-cp -f ${repoDir}/zm-build/rpmconf/Build/get_plat_tag.sh                                 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin
-cp -f ${repoDir}/zm-build/rpmconf/Build/zmValidateLdap.pl                               zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin
-cp -f ${repoDir}/zm-build/rpmconf/Install/Util/addUser.sh                               zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/util
-cp -f ${repoDir}/zm-build/rpmconf/Install/Util/globals.sh                               zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/util
-cp -f ${repoDir}/zm-build/rpmconf/Install/Util/modules/getconfig.sh                     zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/util/modules
-cp -f ${repoDir}/zm-build/rpmconf/Install/Util/modules/packages.sh                      zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/util/modules
-cp -f ${repoDir}/zm-build/rpmconf/Install/Util/modules/postinstall.sh                   zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/util/modules
-cp -f ${repoDir}/zm-build/rpmconf/Install/Util/utilfunc.sh                              zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/util
-cp -f ${repoDir}/zm-build/rpmconf/Install/install.sh                                    zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/
-cp -f ${repoDir}/zm-core-utils/src/libexec/zmdbintegrityreport                          zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin
-cp -f ${repoDir}/zm-store/build/dist/versions-init.sql                                  zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/data
-cp -f ${repoDir}/zm-build/${arch}/*.*                                                   zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/packages
+cp -f ${repoDir}/zm-build/RE/README.txt                                                 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/
+cp -f ${repoDir}/zm-build/rpmconf/Build/checkLicense.pl                                 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin
+cp -f ${repoDir}/zm-build/rpmconf/Build/checkService.pl                                 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin
+cp -f ${repoDir}/zm-build/rpmconf/Build/get_plat_tag.sh                                 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin
+cp -f ${repoDir}/zm-build/rpmconf/Build/zmValidateLdap.pl                               zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin
+cp -f ${repoDir}/zm-build/rpmconf/Install/Util/addUser.sh                               zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/util
+cp -f ${repoDir}/zm-build/rpmconf/Install/Util/globals.sh                               zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/util
+cp -f ${repoDir}/zm-build/rpmconf/Install/Util/modules/getconfig.sh                     zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/util/modules
+cp -f ${repoDir}/zm-build/rpmconf/Install/Util/modules/packages.sh                      zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/util/modules
+cp -f ${repoDir}/zm-build/rpmconf/Install/Util/modules/postinstall.sh                   zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/util/modules
+cp -f ${repoDir}/zm-build/rpmconf/Install/Util/utilfunc.sh                              zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/util
+cp -f ${repoDir}/zm-build/rpmconf/Install/install.sh                                    zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/
+cp -f ${repoDir}/zm-core-utils/src/libexec/zmdbintegrityreport                          zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin
+cp -f ${repoDir}/zm-store/build/dist/versions-init.sql                                  zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/data
+cp -f ${repoDir}/zm-build/${arch}/*.*                                                   zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/packages
 
-chmod 755 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin/checkService.pl
-chmod 755 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin/checkLicense.pl
-chmod 755 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin/zmValidateLdap.pl
-chmod 755 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin/zmdbintegrityreport
-chmod 755 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/install.sh
+chmod 755 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin/checkService.pl
+chmod 755 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin/checkLicense.pl
+chmod 755 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin/zmValidateLdap.pl
+chmod 755 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin/zmdbintegrityreport
+chmod 755 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/install.sh
 
-cp -f ${repoDir}/zm-admin-help-common/WebRoot/help/en_US/admin/pdf/*.pdf                zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/docs/en_US
-cp -f ${repoDir}/zm-admin-help-common/WebRoot/help/en_US/admin/txt/readme_binary.txt    zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/readme_binary_en_US.txt
+cp -f ${repoDir}/zm-admin-help-common/WebRoot/help/en_US/admin/pdf/*.pdf                zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/docs/en_US
+cp -f ${repoDir}/zm-admin-help-common/WebRoot/help/en_US/admin/txt/readme_binary.txt    zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/readme_binary_en_US.txt
 
 if [ "${buildType}" == "NETWORK" ]
 then
-   cp -f ${repoDir}/zm-admin-help-network/WebRoot/help/en_US/admin/pdf/*.pdf               zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/docs/en_US
-   cp -f ${repoDir}/zm-admin-help-network/WebRoot/help/en_US/admin/txt/readme_binary.txt   zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/readme_binary_en_US.txt
+   cp -f ${repoDir}/zm-admin-help-network/WebRoot/help/en_US/admin/pdf/*.pdf               zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/docs/en_US
+   cp -f ${repoDir}/zm-admin-help-network/WebRoot/help/en_US/admin/txt/readme_binary.txt   zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/readme_binary_en_US.txt
 
-   cp -f ${repoDir}/zm-backup-store/build/dist/backup-version-init.sql                     zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/data
-   cp -f ${repoDir}/zm-license-tools/build/zm-license-tools-*.jar                          zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/lib/jars/zimbra-license-tools.jar
+   cp -f ${repoDir}/zm-backup-store/build/dist/backup-version-init.sql                     zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/data
+   cp -f ${repoDir}/zm-license-tools/build/zm-license-tools-*.jar                          zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/lib/jars/zimbra-license-tools.jar
 
-   cp -f ${repoDir}/zm-network-licenses/thirdparty/keyview_eula.txt                        zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/docs/keyview_eula.txt
-   cp -f ${repoDir}/zm-network-licenses/thirdparty/oracle_jdk_eula.txt                     zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/docs/oracle_jdk_eula.txt
-   cp -f ${repoDir}/zm-network-licenses/zimbra/zimbra_network_eula.txt                     zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/docs/zimbra_network_eula.txt
+   cp -f ${repoDir}/zm-network-licenses/thirdparty/keyview_eula.txt                        zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/docs/keyview_eula.txt
+   cp -f ${repoDir}/zm-network-licenses/thirdparty/oracle_jdk_eula.txt                     zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/docs/oracle_jdk_eula.txt
+   cp -f ${repoDir}/zm-network-licenses/zimbra/zimbra_network_eula.txt                     zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/docs/zimbra_network_eula.txt
 
-   cp -f ${repoDir}/zm-network-build/rpmconf/Install/Util/modules/postinstall.sh           zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/util/modules
-   cp -f ${repoDir}/zm-network-build/rpmconf/Util/checkValidBackup                         zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin/checkValidBackup
+   cp -f ${repoDir}/zm-network-build/rpmconf/Install/Util/modules/postinstall.sh           zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/util/modules
+   cp -f ${repoDir}/zm-network-build/rpmconf/Util/checkValidBackup                         zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin/checkValidBackup
 
-   chmod 755 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}/bin/checkValidBackup
+   chmod 755 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}/bin/checkValidBackup
 fi
 
-tar czf zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}.tgz                 zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}
+tar czf zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}.tgz                 zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}
 
-echo "Zcs build completed: ${repoDir}/zm-build/zcs-${buildType}-${release}_${buildNo}.${os}.${buildTimeStamp}.tgz"
+echo "Zcs build completed: ${repoDir}/zm-build/zcs-${buildType}-${releaseNo}_${releaseCandidate}_${buildNo}.${os}.${buildTimeStamp}.tgz"
--- scripts/packages/zimbra-dnscache.sh
+++ scripts/packages/zimbra-dnscache.sh
@@ -55,7 +55,7 @@ CreateDebianPackage()
     echo -e "\tCreate debian package" >> ${buildLogFile}
     (cd ${repoDir}/zm-build/${currentPackage}; find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -print0 | xargs -0 md5sum | sed -e 's| \./| |' \
         > ${repoDir}/zm-build/${currentPackage}/DEBIAN/md5sums)
-    cat ${repoDir}/zm-build/rpmconf/Spec/${currentScript}.deb | sed -e "s/@@VERSION@@/${release}.${buildNo}.${os/_/.}/" \
+    cat ${repoDir}/zm-build/rpmconf/Spec/${currentScript}.deb | sed -e "s/@@VERSION@@/${releaseNo}.${releaseCandidate}.${buildNo}.${os/_/.}/" \
         -e "s/@@branch@@/${buildTimeStamp}/" -e "s/@@ARCH@@/${arch}/" -e "s/^Copyright:/Copyright:/" -e "/^%post$/ r ${currentPackage}.post" \
         > ${repoDir}/zm-build/${currentPackage}/DEBIAN/control
 
@@ -67,7 +67,7 @@ CreateDebianPackage()
 CreateRhelPackage()
 {
     cat ${repoDir}/zm-build/rpmconf/Spec/${currentScript}.spec | \
-        sed -e "s/@@VERSION@@/${release}.${buildNo}.${os}/" \
+        sed -e "s/@@VERSION@@/${releaseNo}_${releaseCandidate}_${buildNo}.${os}/" \
             -e "s/@@RELEASE@@/${buildTimeStamp}/" \
             -e "s/^Copyright:/Copyright:/" \
             -e "/^%post$/ r ${repoDir}/zm-build/rpmconf/Spec/Scripts/${currentScript}.post" > ${repoDir}/zm-build/${currentScript}.spec
