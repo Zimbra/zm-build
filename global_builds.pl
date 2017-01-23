89e7fb37fe6f3d8271c5104dd3f7ca8e2e87e915
Rupali Desai <rupali.desai@synacor.com>
2017-01-23 10:44:17 +0530

    Creating build from smime branch

--- global_builds.pl
+++ global_builds.pl
@@ -813,15 +813,15 @@
       "ant_targets" => ["publish-local"],
        "stage_cmd"   => sub {
          System("mkdir -p $GLOBAL_BUILD_DIR/zm-smime-store/build/dist");
-         System("cp -f -r build/dist $GLOBAL_BUILD_DIR/zm-smime-store/build");
+         System("cp -f -r build $GLOBAL_BUILD_DIR/zm-smime-store/build");
       },
    },
    {
       "dir"         => "zm-securemail-zimlet",
       "ant_targets" => ["package-zimlet"],
       "stage_cmd"   => sub {
-         System("mkdir -p $GLOBAL_BUILD_DIR/zm-securemail-zimlet/build/dist");
-         System("cp -f build/zimlet/com_zimbra_securemail.zip $GLOBAL_BUILD_DIR/zm-securemail-zimlet/build/dist");
+         System("mkdir -p $GLOBAL_BUILD_DIR/zm-securemail-zimlet/build/zimlet");
+         System("cp -f build/zimlet/com_zimbra_securemail.zip $GLOBAL_BUILD_DIR/zm-securemail-zimlet/build/zimlet");
       },
    },
 );
