39f1b07116fd8bf8a5ddd5114c6a18a0308980be
Greg Solovyev <greg.solovyev@synacor.com>
2017-03-17 02:00:48 -0700

    change remote to default

--- instructions/FOSS_remote_list.pl
+++ instructions/FOSS_remote_list.pl
@@ -1,4 +1,4 @@
 @ENTRIES = (
-   "zm"    => { 'url-prefix' => "ssh://git\@stash.corp.synacor.com:7999/~greg.solovyev", },
+   "zm"    => { 'url-prefix' => "ssh://git\@stash.corp.synacor.com:7999/zimbra", },
    "gh-ks" => { 'url-prefix' => "https://github.com/kohlschutter", },
 );
