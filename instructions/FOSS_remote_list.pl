799f73452c4c296a1b79679298337578ab2fd9af
Greg Solovyev <greg.solovyev@synacor.com>
2017-03-17 03:00:33 -0400

    use release integration branch on my fork

--- instructions/FOSS_remote_list.pl
+++ instructions/FOSS_remote_list.pl
@@ -1,4 +1,4 @@
 @ENTRIES = (
-   "zm"    => { 'url-prefix' => "ssh://git\@stash.corp.synacor.com:7999/zimbra", },
+   "zm"    => { 'url-prefix' => "ssh://git\@stash.corp.synacor.com:7999/~greg.solovyev", },
    "gh-ks" => { 'url-prefix' => "https://github.com/kohlschutter", },
 );
