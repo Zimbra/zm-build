23e5278a7046a0e7f9464b93ca428e5376d6c2df
Shriram V <shriram.vishwanathan@synacor.com>
2017-04-03 04:53:13 -0400

    Renamed remote zm as gh-zm

--- instructions/FOSS_remote_list.pl
+++ instructions/FOSS_remote_list.pl
@@ -1,4 +1,4 @@
 @ENTRIES = (
-   "zm"    => { 'url-prefix' => "https://github.com/Zimbra", },
+   "gh-zm" => { 'url-prefix' => "https://github.com/Zimbra", },
    "gh-ks" => { 'url-prefix' => "https://github.com/kohlschutter", },
 );
