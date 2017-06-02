698d15295abea8818e737c611c8cf860df8e2d9a
Shriram V <shriram.vishwanathan@synacor.com>
2017-06-02 14:16:59 -0400

    Minor enhancements to the utility

--- show_git_overrides.sh
+++ show_git_overrides.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]
+then
+   echo "Usage: $0 <cfg|cmd> [remote-branch]"
+   exit 1
+fi
+
 CDPATH=
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 main()
 {
    local CFG="$1"; shift;
+   local BR="$1"; shift;
 
    local i
    for i in $SCRIPT_DIR/../*
@@ -13,45 +20,58 @@ main()
       if [ -d "$i/.git" ]; then
          (
             cd $i;
-            LOCAL_BRANCH=$( git branch -vv | grep '^[*]' | sed -e 's/no branch/no-branch/' | awk '{ print $2 }')
-            REMOTE_NAME=$(  git branch -vv | grep '^[*]' | grep -o '\[[^]]*\]' | sed -e 's,:.*\],],' -e 's,^.,,' -e 's,\]$,,' -e 's,/.*,,')
-            REMOTE_BRANCH=$(git branch -vv | grep '^[*]' | grep -o '\[[^]]*\]' | sed -e 's,:.*\],],' -e 's,^.,,' -e 's,\]$,,' -e 's,.*/,,')
-            REMOTE_URL=$(   git remote get-url "$REMOTE_NAME" 2>/dev/null)
+            if [ "$BR" ]
+            then
+               LOCAL_BRANCH=$( git branch -a | grep suiteplus | grep '^\s*remotes' | sed -e 's,\s*remotes/,,' -e 's,/, ,' | head -1 | awk '{ print $2 }')
+               REMOTE_BRANCH=$LOCAL_BRANCH
+               REMOTE_NAME=
+               REMOTE_URL=
+               REPO_NAME=$(git remote -v | awk '{ print $2; exit; }' | xargs -n1 '-I{}' -- basename '{}' .git)
+            else
+               LOCAL_BRANCH=$( git branch -vv | grep '^[*]' | sed -e 's/no branch/no-branch/' | awk '{ print $2 }')
+               REMOTE_BRANCH=$(git branch -vv | grep '^[*]' | grep -o '\[[^]]*\]' | sed -e 's,:.*\],],' -e 's,^.,,' -e 's,\]$,,' -e 's,.*/,,')
+               REMOTE_NAME=$(  git branch -vv | grep '^[*]' | grep -o '\[[^]]*\]' | sed -e 's,:.*\],],' -e 's,^.,,' -e 's,\]$,,' -e 's,/.*,,')
+               REMOTE_URL=$(   git remote get-url "$REMOTE_NAME" 2>/dev/null)
+               REPO_NAME=$(basename "$REMOTE_URL" .git)
+            fi
 
-            if [ "$LOCAL_BRANCH" == "(no-branch)" ] || [ -z "$REMOTE_NAME" ]
+            if [ "$LOCAL_BRANCH" == "(no-branch)" ] || [ -z "$LOCAL_BRANCH" ] || [ -z "$REPO_NAME" ]
             then
                continue
             fi
 
             if [ "$LOCAL_BRANCH" != "develop" ]
             then
-               if [ "$CFG" ]
+               if [ "$CFG" == "cfg" ]
                then
-                  echo "%GIT_OVERRIDES          = $(basename "$REMOTE_URL" .git).branch=$LOCAL_BRANCH"
+                  echo "%GIT_OVERRIDES          = ${REPO_NAME}.branch=$LOCAL_BRANCH"
                else
-                  echo "--git-overrides '$(basename "$REMOTE_URL" .git).branch=$LOCAL_BRANCH'"
+                  echo "--git-overrides '${REPO_NAME}.branch=$LOCAL_BRANCH'"
                fi
             fi
 
             if [ "$LOCAL_BRANCH" != "$REMOTE_BRANCH" ] && [ "$REMOTE_BRANCH" != "develop" ]
             then
-               if [ "$CFG" ]
+               if [ "$CFG" == "cfg" ]
                then
-                  echo "%GIT_OVERRIDES          = $(basename "$REMOTE_URL" .git).branch=$REMOTE_BRANCH"
+                  echo "%GIT_OVERRIDES          = ${REPO_NAME}.branch=$REMOTE_BRANCH"
                else
-                  echo "# --git-overrides '$(basename "$REMOTE_URL" .git).branch=$REMOTE_BRANCH'"
+                  echo "# --git-overrides '${REPO_NAME}.branch=$REMOTE_BRANCH'"
                fi
             fi
 
-            if ! grep -q -e "/Zimbra" -e ":Zimbra" -e "/zimbra/" <(echo "$REMOTE_URL")
+            if [ "$REMOTE_URL" ]
             then
-               if [ "$CFG" ]
+               if ! grep -q -e "/Zimbra" -e ":Zimbra" -e "/zimbra/" <(echo "$REMOTE_URL")
                then
-                  echo "%GIT_OVERRIDES          = $(basename "$REMOTE_URL" .git).remote=my-$(basename "$REMOTE_URL" .git)-rem"
-                  echo "%GIT_OVERRIDES          = my-$(basename "$REMOTE_URL" .git)-rem.url-prefix=$REMOTE_URL"
-               else
-                  echo "--git-overrides '$(basename "$REMOTE_URL" .git).remote=my-$(basename "$REMOTE_URL" .git)-rem'"
-                  echo "--git-overrides 'my-$(basename "$REMOTE_URL" .git)-rem.url-prefix=$REMOTE_URL'"
+                  if [ "$CFG" == "cfg" ]
+                  then
+                     echo "%GIT_OVERRIDES          = ${REPO_NAME}.remote=my-${REPO_NAME}-rem"
+                     echo "%GIT_OVERRIDES          = my-${REPO_NAME}-rem.url-prefix=$REMOTE_URL"
+                  else
+                     echo "--git-overrides '${REPO_NAME}.remote=my-${REPO_NAME}-rem'"
+                     echo "--git-overrides 'my-${REPO_NAME}-rem.url-prefix=$REMOTE_URL'"
+                  fi
                fi
             fi
          )
