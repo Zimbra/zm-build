18420fdab71931d24f5769394f0dfcf5cb797490
Gren Elliot <gren.elliot@synacor.com>
2018-03-15 00:16:28 +0000

    ZCS-4347:zm-store loses zimbra-license.jar
    This is now build in zm-license-store as the zimbra-license-extension
    package.

--- instructions/bundling-scripts/zimbra-store.sh
+++ instructions/bundling-scripts/zimbra-store.sh
@@ -117,7 +117,6 @@ main()
       cp -f ${repoDir}/zm-twofactorauth-store/build/dist/zm-twofactorauth-store*.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/lib/ext/twofactorauth/zimbratwofactorauth.jar
       cp -f ${repoDir}/zm-hsm-store/build/zimbrahsm.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/lib/ext/zimbrahsm/zimbrahsm.jar
       cp -f ${repoDir}/zm-freebusy-provider-store/build/zimbra-freebusyprovider.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/lib/ext/zimbra-freebusy/zimbra-freebusyprovider.jar
-      cp -f ${repoDir}/zm-license-store/build/dist/zm-license-store*.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/lib/ext/zimbra-license/zimbra-license.jar
       cp -rf ${repoDir}/zm-smime-store/build/dist/*.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/lib/ext/smime
     fi
 
