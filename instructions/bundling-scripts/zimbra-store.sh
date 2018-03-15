a770203207477812f4abb4ccb07e8a1add586790
rupalid <rtdesai@gmail.com>
2018-03-15 15:58:14 +0530

    ZCS-4355 Remove core and lib jars from build (#86)

--- instructions/bundling-scripts/zimbra-store.sh
+++ instructions/bundling-scripts/zimbra-store.sh
@@ -274,7 +274,6 @@ main()
       cp -f ${repoDir}/zm-zcs-lib/build/dist/bcprov-jdk15on-1.55.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/lib/ext-common/
     fi
 
-    cp -f ${repoDir}/zm-zcs-lib/build/dist/zmzimbratozimbramig-8.7.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/lib/jars/zmzimbratozimbramig.jar
     cp -f ${repoDir}/zm-migration-tools/src/libexec/zmztozmig ${repoDir}/zm-build/${currentPackage}/opt/zimbra/libexec
     cp -f ${repoDir}/zm-migration-tools/src/libexec/zmcleaniplanetics ${repoDir}/zm-build/${currentPackage}/opt/zimbra/libexec
     cp -f ${repoDir}/zm-versioncheck-utilities/src/libexec/zmcheckversion ${repoDir}/zm-build/${currentPackage}/opt/zimbra/libexec
@@ -330,68 +329,6 @@ main()
     mkdir -p ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/common/endorsed
     mkdir -p ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/common/lib
 
-    cp ${repoDir}/zm-zcs-lib/build/dist/jcharset-2.0.jar  ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/common/endorsed/jcharset.jar
-    cp ${repoDir}/zm-zcs-lib/build/dist/zm-charset-*.jar  ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/common/endorsed/zimbra-charset.jar
-
-    zimbraPatchedJars=("ant-1.7.0-ziputil-patched.jar" "ical4j-0.9.16-patched.jar" "nekohtml-1.9.13.1z.jar");
-    for i in "${zimbraPatchedJars[@]}"
-    do
-        cp ${repoDir}/zm-zcs-lib/build/dist/${i} ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/common/lib
-    done
-
-    thirdpartyJars=(
-        "apache-log4j-extras-1.0.jar"
-        "bcprov-jdk15on-1.55.jar"
-        "commons-cli-1.2.jar"
-        "commons-codec-1.7.jar"
-        "commons-collections-3.2.2.jar"
-        "commons-compress-1.10.jar"
-        "commons-dbcp-1.4.jar"
-        "commons-fileupload-1.2.2.jar"
-        "commons-httpclient-3.1.jar"
-        "commons-io-1.4.jar"
-        "commons-lang-2.6.jar"
-        "commons-logging-1.1.1.jar"
-        "commons-net-3.3.jar"
-        "commons-pool-1.6.jar"
-        "concurrentlinkedhashmap-lru-1.3.1.jar"
-        "dom4j-1.5.2.jar"
-        "ganymed-ssh2-build210.jar"
-        "guava-13.0.1.jar"
-        "httpasyncclient-4.1.2.jar"
-        "httpclient-4.5.2.jar"
-        "httpcore-4.4.5.jar"
-        "httpcore-nio-4.4.5.jar"
-	"httpmime-4.3.1.jar"
-        "icu4j-4.8.1.1.jar"
-        "jaxen-1.1.3.jar"
-        "jcommon-1.0.21.jar"
-        "jdom-1.1.jar"
-        "jfreechart-1.0.15.jar"
-        "json-20090211.jar"
-        "jtnef-1.9.0.jar"
-        "junixsocket-common-2.0.4.jar"
-        "junixsocket-demo-2.0.4.jar"
-        "junixsocket-mysql-2.0.4.jar"
-        "junixsocket-rmi-2.0.4.jar"
-        "jzlib-1.0.7.jar"
-        "libidn-1.24.jar"
-        "log4j-1.2.16.jar"
-        "mail-1.4.5.jar"
-        "mariadb-java-client-1.1.8.jar"
-        "oauth-20100527.jar"
-        "spymemcached-2.12.1.jar"
-        "unboundid-ldapsdk-2.3.5.jar"
-        "xercesImpl-2.9.1-patch-01.jar"
-        "yuicompressor-2.4.2-zimbra.jar"
-    )
-    for i in "${thirdpartyJars[@]}"
-    do
-        cp ${repoDir}/zm-zcs-lib/build/dist/${i} ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/common/lib
-    done
-
-   cp ${repoDir}/zm-zcs-lib/build/dist/zm-common-*.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/common/lib/zimbracommon.jar
-   cp ${repoDir}/zm-zcs-lib/build/dist/zm-native-*.jar ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/common/lib/zimbra-native.jar
 
    mkdir -p ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/temp
    touch ${repoDir}/zm-build/${currentPackage}/opt/zimbra/jetty_base/temp/.emptyfile
