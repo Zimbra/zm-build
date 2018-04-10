265a23c2a93c6035f51b1931f65c5df2ce0c8b23
Ajinkya Nahar <ajinkya.nahar@synacor.com>
2018-04-10 04:27:57 +0000

    adding zm-network-store in FOSS build scripts

--- instructions/FOSS_staging_list.pl
+++ instructions/FOSS_staging_list.pl
@@ -388,13 +388,9 @@
          SysExec("cp -f -rp build/zm-oauth-social-*.jar $CFG{BUILD_DIR}/zm-oauth-social/build/dist");
       },
    },
-   
    {
-      "dir"         => "zm-gql",
-      "ant_targets" => ["publish-local"],
-      "stage_cmd"   => sub {
-         SysExec("mkdir -p $CFG{BUILD_DIR}/zm-gql/build/dist");
-         SysExec("cp -f -rp build/zm-gql-*.jar $CFG{BUILD_DIR}/zm-gql/build/dist");
-      },
+      "dir"         => "zm-network-store",
+      "ant_targets" => [ "publish-local", "pkg" ],
+      "deploy_pkg_into" => "bundle",
    },
 );
