193d812a51e994c55eec2e8a116e51e499d86562
Ajinkya Nahar <ajinkya.nahar@synacor.com>
2018-04-16 05:43:02 +0000

    Revert "adding zm-network-store in FOSS build scripts"
    This reverts commit 1dede74deccd20c4a05cf74f638f1a0ebbf8f29d.

--- instructions/FOSS_remote_list.pl
+++ instructions/FOSS_remote_list.pl
@@ -1,5 +1,4 @@
 @ENTRIES = (
    "gh-zm" => { 'url-prefix' => "https://github.com/Zimbra", },
    "gh-ks" => { 'url-prefix' => "https://github.com/kohlschutter", },
-   "zm-nw" => { 'url-prefix' => q{git@github.com:Zimbra}, },
 );
