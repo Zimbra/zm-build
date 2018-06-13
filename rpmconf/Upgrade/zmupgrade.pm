0e2d7bb6959a019e62d920ee8872b07bf5913190
ilya <raykini@zimbra.com>
2018-06-13 15:36:53 -0700

    ZCS-5192: added migration script for db version 111, bumped hiVersion to 112

--- rpmconf/Upgrade/zmupgrade.pm
+++ rpmconf/Upgrade/zmupgrade.pm
@@ -41,7 +41,7 @@ chomp $rundir;
 my $scriptDir = "/opt/zimbra/libexec/scripts";
 
 my $lowVersion = 52;
-my $hiVersion = 111; # this should be set to the DB version expected by current server code
+my $hiVersion = 112; # this should be set to the DB version expected by current server code
 
 my $needSlapIndexing = 0;
 my $mysqlcnfUpdated = 0;
@@ -87,6 +87,7 @@ my %updateScripts = (
   '108' => "migrate20180301-ZimbraChat.pl",            #8.8.8
   '109' => "migrate20170829-SearchHistory.pl",
   '110' => "migrate20180110-DistributedRedolog.pl",
+  '111' => "migrate20180613-EventFlagColumn.pl",
 );
 
 my %updateFuncs = (
