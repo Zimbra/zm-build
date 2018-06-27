362c1335834888f29699345c66474f25ee86e3cf
log2akshat <akshat-pg8@iiitmk.ac.in>
2018-06-27 13:59:40 +0530

    Removed prompt for installing zimbra-patch, it should be installed by default.

--- rpmconf/Install/Util/utilfunc.sh
+++ rpmconf/Install/Util/utilfunc.sh
@@ -2392,6 +2392,8 @@ getInstallPackages() {
 
     if [ $i = "zimbra-license-tools" ]; then
       response="yes"
+    elif [ $i = "zimbra-patch" ]; then
+      response="yes"
     elif [ $i = "zimbra-license-extension" ]; then
       ifStoreSelectedY
     elif [ $i = "zimbra-network-store" ]; then
