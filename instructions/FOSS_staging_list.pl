a575a09f844fd3e1e101eb4f8fa0b3a59f773ac9
rupalid <rtdesai@gmail.com>
2018-08-09 11:57:13 +0530

    ZCS-5738 Building zm-gql extension (#129)

--- instructions/FOSS_staging_list.pl
+++ instructions/FOSS_staging_list.pl
@@ -388,4 +388,13 @@
          SysExec("cp -f -rp build/zm-oauth-social-*.jar $CFG{BUILD_DIR}/zm-oauth-social/build/dist");
       },
    },
+   
+   {
+      "dir"         => "zm-gql",
+      "ant_targets" => ["publish-local"],
+      "stage_cmd"   => sub {
+         SysExec("mkdir -p $CFG{BUILD_DIR}/zm-gql/build/dist");
+         SysExec("cp -f -rp build/zm-gql-*.jar $CFG{BUILD_DIR}/zm-gql/build/dist");
+      },
+   },
 );
