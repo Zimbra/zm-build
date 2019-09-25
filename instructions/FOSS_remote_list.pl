905970576d6fe337150f09c0ad7a0f53aa1a8f42
sneha-patil-synacor <sneha.patil@synacor.com>
2019-09-25 13:44:00 +0530

    adding remote for owasp repo entry

--- instructions/FOSS_remote_list.pl
+++ instructions/FOSS_remote_list.pl
@@ -1,4 +1,5 @@
 @ENTRIES = (
    "gh-zm" => { 'url-prefix' => "https://github.com/Zimbra", },
    "gh-ks" => { 'url-prefix' => "https://github.com/kohlschutter", },
+   "zm-ow"  => { 'url-prefix' => q{git@github.com:Zimbra}, },
 );
