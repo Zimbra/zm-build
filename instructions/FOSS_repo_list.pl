6695dc83aebb53b575a2f6563aab1f3105015ef6
Nilesh Pawar <nilesh.pawar@synacor.com>
2019-12-18 13:07:53 +0530

    ZCS-8339: add support for jdk13

--- instructions/FOSS_repo_list.pl
+++ instructions/FOSS_repo_list.pl
@@ -2,7 +2,7 @@
    { name => "ant-1.7.0-ziputil-patched",            },
    { name => "ant-tar-patched",                      },
    { name => "ical4j-0.9.16-patched",                },
-   { name => "junixsocket",                         tag    => "junixsocket-parent-2.0.4", remote => "gh-ks",},
+   { name => "junixsocket",                         tag    => "junixsocket-parent-2.2.1", remote => "gh-ks",},
    { name => "nekohtml-1.9.13",                      },
    { name => "java-html-sanitizer-release-20190610.1",remote => "zm-ow",},
    { name => "antisamy", remote => "zm-ow",          },
