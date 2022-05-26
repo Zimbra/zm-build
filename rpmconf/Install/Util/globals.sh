8554e7f760491df5d6aee56458dd8adc81a9d3a2
UmaShankar Avagadda <umagmrit@gmail.com>
2022-05-26 10:02:38 +0000

    ZCS-11304:Changes for 9.0.0 FOSS Build

--- rpmconf/Install/Util/globals.sh
+++ rpmconf/Install/Util/globals.sh
@@ -39,9 +39,6 @@ SERVICES=""
 
 OPTIONAL_PACKAGES="zimbra-qatest \
 zimbra-imapd \
-zimbra-patch \
-zimbra-mta-patch \
-zimbra-proxy-patch \
 zimbra-license-tools \
 zimbra-license-extension \
 zimbra-network-store"
@@ -59,7 +56,7 @@ zimbra-network-modules-ng"
 
 MYDIR="$(CDPATH= cd "$(dirname "$0")" && pwd)"
 if [ "$(cat ${MYDIR}/.BUILD_TYPE)" == "NETWORK" ]; then
-   OPTIONAL_PACKAGES="${OPTIONAL_PACKAGES} zimbra-modern-ui zimbra-modern-zimlets"
+   OPTIONAL_PACKAGES="${OPTIONAL_PACKAGES} zimbra-modern-ui zimbra-modern-zimlets zimbra-patch zimbra-mta-patch zimbra-proxy-patch"
 fi
 
 PACKAGE_DIR="$(CDPATH= cd "$(dirname "$0")" && pwd)/packages"
