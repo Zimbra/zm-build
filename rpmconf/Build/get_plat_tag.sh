b3600e39c46c1af0297e791de40726c28154e0f3
UmaShankar Avagadda <umagmrit@gmail.com>
2023-12-07 05:48:48 +0000

    ZCS-13984:Changes for RHEL9

--- rpmconf/Build/get_plat_tag.sh
+++ rpmconf/Build/get_plat_tag.sh
@@ -25,6 +25,11 @@ if [ -f /etc/redhat-release ]; then
     i=""
   fi
 
+  grep "Red Hat Enterprise Linux.*release 9" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL9${i}"
+    exit 0
+  fi
   grep "Red Hat Enterprise Linux.*release 8" /etc/redhat-release > /dev/null 2>&1
   if [ $? = 0 ]; then
     echo "RHEL8${i}"
@@ -41,6 +46,11 @@ if [ -f /etc/redhat-release ]; then
     exit 0
   fi
 
+  grep "CentOS Linux release 9" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL9${i}"
+    exit 0
+  fi
   grep "CentOS Linux release 8" /etc/redhat-release > /dev/null 2>&1
   if [ $? = 0 ]; then
     echo "RHEL8${i}"
@@ -57,12 +67,22 @@ if [ -f /etc/redhat-release ]; then
     exit 0
   fi
 
+  grep "Rocky Linux release 9" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL9${i}"
+    exit 0
+  fi
   grep "Rocky Linux release 8" /etc/redhat-release > /dev/null 2>&1
   if [ $? = 0 ]; then
     echo "RHEL8${i}"
     exit 0
   fi
 
+  grep "Scientific Linux release 9" /etc/redhat-release > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "RHEL9${i}"
+    exit 0
+  fi
   grep "Scientific Linux release 8" /etc/redhat-release > /dev/null 2>&1
   if [ $? = 0 ]; then
     echo "RHEL8${i}"
