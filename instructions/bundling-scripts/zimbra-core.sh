4faf467bf8a58b2ba0766d93cc5299de36be8dd4
UmaShankar Avagadda <umagmrit@gmail.com>
2024-11-19 14:32:59 +0000

    ZCS-16124 : add zmacl

--- instructions/bundling-scripts/zimbra-core.sh
+++ instructions/bundling-scripts/zimbra-core.sh
@@ -303,6 +303,7 @@ main()
    Copy ${repoDir}/zm-core-utils/src/bin/zmrabbitmqctl                                              ${repoDir}/zm-build/${currentPackage}/opt/zimbra/bin/zmrabbitmqctl
    Copy ${repoDir}/zm-core-utils/src/bin/zmonlyofficeinstall                                        ${repoDir}/zm-build/${currentPackage}/opt/zimbra/bin/zmonlyofficeinstall
    Copy ${repoDir}/zm-core-utils/src/bin/zmlicensectl                                               ${repoDir}/zm-build/${currentPackage}/opt/zimbra/bin/zmlicensectl
+   Copy ${repoDir}/zm-core-utils/src/bin/zmacl                                                      ${repoDir}/zm-build/${currentPackage}/opt/zimbra/bin/zmacl
    Copy ${repoDir}/zm-core-utils/src/contrib/zmfetchercfg                                           ${repoDir}/zm-build/${currentPackage}/opt/zimbra/contrib/zmfetchercfg
    Copy ${repoDir}/zm-core-utils/src/libexec/600.zimbra                                             ${repoDir}/zm-build/${currentPackage}/opt/zimbra/libexec/600.zimbra
    Copy ${repoDir}/zm-core-utils/src/libexec/client_usage_report.py                                 ${repoDir}/zm-build/${currentPackage}/opt/zimbra/libexec/client_usage_report.py
