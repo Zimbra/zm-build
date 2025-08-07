6d2849439975df7450b2fd34efad7ddad9c813c1
UmaShankar Avagadda <umagmrit@gmail.com>
2025-08-07 13:36:29 +0000

    ZCS-17688 : added u24 support

--- rpmconf/Build/get_plat_tag.sh
+++ rpmconf/Build/get_plat_tag.sh
@@ -198,6 +198,10 @@ if [ -f /etc/lsb-release ]; then
       echo "22${i}"
       exit 0
     fi
+    if [ "$RELEASE" = "noble" ]; then
+      echo "24${i}"
+      exit 0
+    fi
     echo "UNKNOWN${i}"
     exit 0
   fi
