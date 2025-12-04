6732ef6dd3519de1990dadb9ebdf163c24f0b852
Chetanlanje <chetanlanje74@gmail.com>
2025-12-04 14:44:03 +0530

    ZCS-18299:Support multiple ANT options in build.pl

--- build.pl
+++ build.pl
@@ -508,7 +508,7 @@ sub Build($)
       ],
    };
 
-   push( @{ $tool_attributes->{ant} }, $CFG{ANT_OPTIONS} )
+   push( @{ $tool_attributes->{ant} }, split(/\s+/, $CFG{ANT_OPTIONS}) )
      if ( $CFG{ANT_OPTIONS} );
 
    push( @{ $tool_attributes->{mvn} }, $CFG{MVN_OPTIONS} )
@@ -555,15 +555,8 @@ sub Build($)
                      {
                         if ( my $targets = $build_info->{ $tool . "_targets" } )    #Known values are: ant_targets, mvn_targets, make_targets
                         {
-                           if ($tool eq 'ant' && $CFG{ANT_OPTIONS}) {
-                               my $opts = $CFG{ANT_OPTIONS};
-                               if ($opts =~ /-DskipCoverage=true/) {
-                                   @$targets = grep { $_ ne 'coverage' } @$targets;
-                                   print "Skipping coverage target due to -DskipCoverage=true\n";
-                               }
-                           }
                            eval { SysExec( $tool, "clean" ) if ( !$ENV{ENV_SKIP_CLEAN_FLAG} ); };
-                           
+
                            SysExec( $tool, @{ $tool_attributes->{$tool} || [] }, @$targets );
                         }
                      }
